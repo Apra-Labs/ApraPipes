3e506f0d7b2576250e9981c7da2f4ccf58abb7e7
sai <sai>
2021-02-01 18:34:46 +0530

    adding parallel compiling flag

--- build_linux_x64.sh
+++ build_linux_x64.sh
@@ -1,11 +1,11 @@
 mkdir -p _build
 cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../base 
-cmake --build .
+cmake --build . -- -j "$(nproc)"
 
 cd ..
 
 mkdir -p _debugbuild
 cd _debugbuild
 cmake -DCMAKE_BUILD_TYPE=Debug ../base 
-cmake --build .
+cmake --build . -- -j "$(nproc)"
