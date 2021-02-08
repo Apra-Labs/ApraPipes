7ecbf8dd10b70f520744131eed63734c3a614bf3
sai <sai>
2021-02-08 15:21:13 +0530

    updating test params for testing

--- base/test/rtsppusher_tests.cpp
+++ base/test/rtsppusher_tests.cpp
@@ -12,13 +12,14 @@ BOOST_AUTO_TEST_SUITE(rtsppusher_tests)
 
 BOOST_AUTO_TEST_CASE(basic, *boost::unit_test::disabled())
 {
-	FileReaderModuleProps fileReaderProps("./data/h264_640x360");
+	FileReaderModuleProps fileReaderProps("./data/ArgusCamera");
 	fileReaderProps.fps = 30;
+	fileReaderProps.maxFileSize = 200*1024;
 	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(fileReaderProps));
 	auto metadata = framemetadata_sp(new FrameMetadata(FrameMetadata::H264_DATA));
 	fileReader->addOutputPin(metadata);
 
-	auto sink = boost::shared_ptr<Module>(new RTSPPusher(RTSPPusherProps("rtsp://10.102.10.129:5544", "aprapipes_h264")));
+	auto sink = boost::shared_ptr<Module>(new RTSPPusher(RTSPPusherProps("rtsp://10.102.10.129:5544", "hola")));
 	fileReader->setNext(sink);
 
 	PipeLine p("test");
@@ -26,7 +27,7 @@ BOOST_AUTO_TEST_CASE(basic, *boost::unit_test::disabled())
 	BOOST_TEST(p.init());
 	p.run_all_threaded();
 
-	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+	boost::this_thread::sleep_for(boost::chrono::seconds(500000000000000));
 
 	LOG_ERROR << "STOPPING";
 
