1d1ed4bc9b5e3c7158b45fa075ddf70e01d3c697
yashraj <39464212+yashrajsingh1998@users.noreply.github.com>
2021-02-26 14:45:04 +0530

    Update build_linux_x64.sh

--- build_linux_x64.sh
+++ build_linux_x64.sh
@@ -3,9 +3,9 @@ cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../base 
 cmake --build . -- -j "$(nproc)"
 
-cd ..
+# cd ..
 
-mkdir -p _debugbuild
-cd _debugbuild
-cmake -DCMAKE_BUILD_TYPE=Debug ../base 
-cmake --build . -- -j "$(nproc)"
+# mkdir -p _debugbuild
+# cd _debugbuild
+# cmake -DCMAKE_BUILD_TYPE=Debug ../base 
+# cmake --build . -- -j "$(nproc)"
