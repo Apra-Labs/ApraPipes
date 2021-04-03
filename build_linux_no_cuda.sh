3dfe03946738cc6c5a6d4a1163f3a46b3b0ee224
sai <saice@apra.in>
2021-04-03 14:02:19 +0530

    fixes to build on machine without Nvidia CUDA

---
+++ build_linux_no_cuda.sh
@@ -0,0 +1,11 @@
+mkdir -p _build
+cd _build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=OFF  ../base 
+cmake --build . -- -j "$(nproc)"
+
+cd ..
+
+mkdir -p _debugbuild
+cd _debugbuild
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=OFF ../base 
+cmake --build . -- -j "$(nproc)"
