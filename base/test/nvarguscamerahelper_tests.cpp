57614f9e94b2e0818d4175664789974d696c3e7a
sai <saice@apra.in>
2021-04-13 16:47:05 +0530

    reporduced the write error in a unit test

--- base/test/nvarguscamerahelper_tests.cpp
+++ base/test/nvarguscamerahelper_tests.cpp
@@ -8,6 +8,8 @@
 #include "test_utils.h"
 #include "Logger.h"
 
+#include <fstream>
+
 BOOST_AUTO_TEST_SUITE(nvarguscamerahelper_tests)
 
 BOOST_AUTO_TEST_CASE(memory_alloc_test, *boost::unit_test::disabled())
@@ -24,6 +26,43 @@ BOOST_AUTO_TEST_CASE(memory_alloc_test, *boost::unit_test::disabled())
     if (frame.get())
     {
         BOOST_TEST(frame->size() == size);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(save_disk_test, *boost::unit_test::disabled())
+{
+    uint32_t width = 1280;
+    uint32_t height = 720;
+    size_t size = width * height * 3 >> 1;
+
+    auto metadata = framemetadata_sp(new RawImagePlanarMetadata(width, height, ImageMetadata::ImageType::NV12, size_t(0), CV_8U, FrameMetadata::MemType::DMABUF));
+    auto frameFactory = framefactory_sp(new FrameFactory(metadata, 10));
+    frameFactory = framefactory_sp(new FrameFactory(metadata, 10));
+
+    std::vector<frame_sp> frames;
+    for (auto i = 0; i < 100; i++)
+    {
+        if (i % 10 == 0)
+        {
+            frames.clear();
+        }
+        auto frame = frameFactory->create(size, frameFactory);
+        if (!frame.get())
+        {
+            continue;
+        }
+
+        std::ofstream file("data/testOutput/hola.bin", std::ios::out | std::ios::binary);
+        if (file.is_open())
+        {
+
+            auto ptr = static_cast<DMAFDWrapper *>(frame->data());
+            file.write((const char *)ptr->getHostPtr(), frame->size());
+            LOG_ERROR << i << "<>" << file.bad() << "<>" << file.eof() << "<>" << file.fail();
+
+            file.close();
+        }
+        frames.push_back(frame);
     }
 }
 
