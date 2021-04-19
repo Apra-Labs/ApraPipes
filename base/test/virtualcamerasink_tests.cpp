593483d977709d2c225c4516acb630d4a64302da
sai <sai>
2021-04-19 09:02:23 +0530

    added tests for nv12, bgra NV12 and BGRA are not working

--- base/test/virtualcamerasink_tests.cpp
+++ base/test/virtualcamerasink_tests.cpp
@@ -83,4 +83,74 @@ BOOST_AUTO_TEST_CASE(yuv420, *boost::unit_test::disabled())
 	p.wait_for_all();
 }
 
+BOOST_AUTO_TEST_CASE(nv12, *boost::unit_test::disabled())
+{
+	LoggerProps logprops;
+	logprops.logLevel = boost::log::trivial::severity_level::info;
+	Logger::initLogger(logprops);
+
+	// metadata is known
+	auto width = 1280;
+	auto height = 720;
+
+	FileReaderModuleProps fileReaderProps("./data/Raw_NV12_1280x720");
+	fileReaderProps.fps = 30;
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(fileReaderProps));	
+	auto metadata = framemetadata_sp(new RawImagePlanarMetadata(width, height, ImageMetadata::ImageType::NV12, size_t(0), CV_8U, FrameMetadata::MemType::HOST));
+
+	fileReader->addOutputPin(metadata);
+
+	VirtualCameraSinkProps sinkProps("/dev/video10");
+	// sinkProps.logHealth = true;
+	// sinkProps.logHealthFrequency = 100;
+	auto sink = boost::shared_ptr<Module>(new VirtualCameraSink(sinkProps));
+	fileReader->setNext(sink);	
+
+	PipeLine p("test");
+	p.appendModule(fileReader);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(1000));
+	LOG_INFO << "profiling done - stopping the pipeline";
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(bgra, *boost::unit_test::disabled())
+{
+	LoggerProps logprops;
+	logprops.logLevel = boost::log::trivial::severity_level::info;
+	Logger::initLogger(logprops);
+
+	// metadata is known
+	auto width = 1280;
+	auto height = 720;
+
+	FileReaderModuleProps fileReaderProps("./data/Raw_BGRA_1280x720");
+	fileReaderProps.fps = 30;
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(fileReaderProps));	
+	auto metadata = framemetadata_sp(new RawImageMetadata(width, height, ImageMetadata::BGRA, CV_8UC4, width*4, CV_8U, FrameMetadata::HOST));
+
+	fileReader->addOutputPin(metadata);
+
+	VirtualCameraSinkProps sinkProps("/dev/video10");
+	// sinkProps.logHealth = true;
+	// sinkProps.logHealthFrequency = 100;
+	auto sink = boost::shared_ptr<Module>(new VirtualCameraSink(sinkProps));
+	fileReader->setNext(sink);	
+
+	PipeLine p("test");
+	p.appendModule(fileReader);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(1000));
+	LOG_INFO << "profiling done - stopping the pipeline";
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
 BOOST_AUTO_TEST_SUITE_END()
