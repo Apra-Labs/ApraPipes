604ae27555d82221b656f8d56aae57059d336e00
yashrajsapra <88146397+yashrajsapra@users.noreply.github.com>
2021-09-15 11:24:54 +0530

    Added VirtualPTZ Module and BrightnessContrast Module (#31)
    * Added
    1. Brightness Contrast Module
    2. VirtualPTZ module
    
    * Modified tests
    
    * Added Logs if Roi is out of bound
    
    * MOdified Set Props logic
    
    * Update BrightnessContrastControl.cpp
    
    * Update virtualptz_tests.cpp

---
+++ base/test/brightness_contrast_tests.cpp
@@ -0,0 +1,122 @@
+#include "stdafx.h"
+#include <boost/test/unit_test.hpp>
+#include "FileReaderModule.h"
+#include "ExternalSinkModule.h"
+#include "FrameMetadata.h"
+#include "FrameMetadataFactory.h"
+#include "Frame.h"
+#include "Logger.h"
+#include "AIPExceptions.h"
+#include "MetadataHints.h"
+#include "test_utils.h"
+#include "FramesMuxer.h"
+#include "PipeLine.h"
+#include "StatSink.h"
+#include "PipeLine.h"
+#include "FileWriterModule.h"
+#include "CudaMemCopy.h"
+#include "BrightnessContrastControl.h"
+
+BOOST_AUTO_TEST_SUITE(brightnes_contrast_tests)
+
+BOOST_AUTO_TEST_CASE(mono)
+{
+
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/mono_1920x1080.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(1920, 960, ImageMetadata::ImageType::MONO, CV_8UC1, 0, CV_8U, FrameMetadata::HOST, true));
+	fileReader->addOutputPin(metadata);
+
+	auto brightnessControl = boost::shared_ptr<BrightnessContrastControl>(new BrightnessContrastControl(BrightnessContrastControlProps(0.5, 40)));
+	fileReader->setNext(brightnessControl);
+
+	auto m2 = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+	brightnessControl->setNext(m2);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(brightnessControl->init());
+	BOOST_TEST(m2->init());
+
+	fileReader->step();
+	brightnessControl->step();
+	auto frames = m2->pop();
+	BOOST_TEST(frames.size() == 1);
+	auto outputFrame = frames.cbegin()->second;
+	BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+	Test_Utils::saveOrCompare("./data/testOutput/brightnessmono.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(bgra)
+{
+	LoggerProps logprops;
+	logprops.logLevel = boost::log::trivial::severity_level::info;
+	Logger::initLogger(logprops);
+
+	auto width = 1920;
+	auto height = 960;
+
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/overlay_1920x960_BGRA.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(width, height, ImageMetadata::BGRA, CV_8UC4, 0, CV_8U, FrameMetadata::HOST));
+	auto rawImagePin = fileReader->addOutputPin(metadata);
+
+	auto brightnessControl = boost::shared_ptr<BrightnessContrastControl>(new BrightnessContrastControl(BrightnessContrastControlProps(0.5, 40)));
+	fileReader->setNext(brightnessControl);
+
+	auto m2 = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+	brightnessControl->setNext(m2);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(brightnessControl->init());
+	BOOST_TEST(m2->init());
+
+	fileReader->step();
+	brightnessControl->step();
+	auto frames = m2->pop();
+	BOOST_TEST(frames.size() == 1);
+	auto outputFrame = frames.cbegin()->second;
+	BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+	Test_Utils::saveOrCompare("./data/testOutput/brightnessbgra.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(getSetProps)
+{
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/frame_1280x720_rgb.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(1280, 720, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
+	fileReader->addOutputPin(metadata);
+
+	auto brightnessControl = boost::shared_ptr<BrightnessContrastControl>(new BrightnessContrastControl(BrightnessContrastControlProps(0.5, 40)));
+	fileReader->setNext(brightnessControl);
+
+	auto m2 = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+	brightnessControl->setNext(m2);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(brightnessControl->init());
+	BOOST_TEST(m2->init());
+
+	{
+		fileReader->step();
+		brightnessControl->step();
+		auto frames = m2->pop();
+		BOOST_TEST(frames.size() == 1);
+		auto outputFrame = frames.cbegin()->second;
+		BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+		Test_Utils::saveOrCompare("./data/testOutput/brightnessgetset1.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+	}
+
+	BrightnessContrastControlProps props12(2, 40);
+
+	brightnessControl->setProps(props12);
+	brightnessControl->step();
+
+	{
+		fileReader->step();
+		brightnessControl->step();
+		auto frames = m2->pop();
+		BOOST_TEST(frames.size() == 1);
+		auto outputFrame = frames.cbegin()->second;
+		BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+		Test_Utils::saveOrCompare("./data/testOutput/brightnessgetset2.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ base/test/virtualptz_tests.cpp
@@ -0,0 +1,138 @@
+#include "stdafx.h"
+#include <boost/test/unit_test.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+#include "VirtualPTZ.h"
+#include "FileReaderModule.h"
+#include "ExternalSinkModule.h"
+#include "FrameMetadata.h"
+#include "FrameMetadataFactory.h"
+#include "Frame.h"
+#include "Logger.h"
+#include "AIPExceptions.h"
+#include "MetadataHints.h"
+#include "test_utils.h"
+#include "ImageResizeCV.h"
+#include "RotateCV.h"
+#include "FramesMuxer.h"
+#include "PipeLine.h"
+#include "StatSink.h"
+#include "ImageOverlayCPU.h"
+#include "TextOverlayCPU.h"
+#include "OpencvWebcam.h"
+#include "StatSink.h"
+#include "PipeLine.h"
+#include "FileWriterModule.h"
+#include "CudaMemCopy.h"
+#include "JPEGEncoderNVJPEG.h"
+#include "ImageEncoderCV.h"
+#include "BrightnessContrastControl.h"
+#include "ImageOverlay.h"
+#include "WebCamSrc.h"
+
+BOOST_AUTO_TEST_SUITE(virtual_ptz_tests)
+
+BOOST_AUTO_TEST_CASE(mono)
+{
+
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/mono_1920x960.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(1920, 960, ImageMetadata::ImageType::MONO, CV_8UC1, 0, CV_8U, FrameMetadata::HOST, true));
+	fileReader->addOutputPin(metadata);
+
+	auto vPtz = boost::shared_ptr<VirtualPTZ>(new VirtualPTZ(VirtualPTZProps(0.09, 0.500, 0.00, 0.00)));
+	fileReader->setNext(vPtz);
+
+	auto Vptzprops = vPtz->getProps();
+
+	auto sink = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+	vPtz->setNext(sink);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(vPtz->init());
+	BOOST_TEST(sink->init());
+
+	fileReader->step();
+	vPtz->step();
+	auto frames = sink->pop();
+	BOOST_TEST(frames.size() == 1);
+	auto outputFrame = frames.cbegin()->second;
+	BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+
+	Test_Utils::saveOrCompare("./data/testOutput/vPtztestmono.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(rgb)
+{
+
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/frame_1280x720_rgb.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(1280, 720, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
+	fileReader->addOutputPin(metadata);
+
+	auto vPtz = boost::shared_ptr<VirtualPTZ>(new VirtualPTZ(VirtualPTZProps(0.29, 0.2900, 0.0, 0.0)));
+	fileReader->setNext(vPtz);
+
+	auto Vptzprops = vPtz->getProps();
+
+	auto sink = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+	vPtz->setNext(sink);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(vPtz->init());
+	BOOST_TEST(sink->init());
+
+	fileReader->step();
+	vPtz->step();
+	auto frames = sink->pop();
+	BOOST_TEST(frames.size() == 1);
+	auto outputFrame = frames.cbegin()->second;
+	BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+
+	Test_Utils::saveOrCompare("./data/testOutput/vPtztestrgb.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(testGetSetProps)
+{
+
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/mono_1920x960.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(1920, 960, ImageMetadata::ImageType::MONO, CV_8UC1, 0, CV_8U, FrameMetadata::HOST, true));
+	fileReader->addOutputPin(metadata);
+
+	auto vPtz = boost::shared_ptr<VirtualPTZ>(new VirtualPTZ(VirtualPTZProps(0.300, 0.300, 0.100, 0.100)));
+	fileReader->setNext(vPtz);
+
+	auto Vptzprops = vPtz->getProps();
+
+	auto sink = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+	vPtz->setNext(sink);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(vPtz->init());
+	BOOST_TEST(sink->init());
+
+	{
+		fileReader->step();
+		vPtz->step();
+		auto frames = sink->pop();
+		BOOST_TEST(frames.size() == 1);
+		auto outputFrame = frames.cbegin()->second;
+		BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+		Test_Utils::saveOrCompare("./data/testOutput/vPtztestprops1.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+	}
+
+	VirtualPTZProps props12(0.950, 0.950, 0.90, 0.90);
+
+	vPtz->setProps(props12);
+	vPtz->step();
+
+	{
+		auto Vptzprops1 = vPtz->getProps();
+		fileReader->step();
+		vPtz->step();
+		auto frames = sink->pop();
+		BOOST_TEST(frames.size() == 1);
+		auto outputFrame = frames.cbegin()->second;
+		BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+		Test_Utils::saveOrCompare("./data/testOutput/vPtztestprops2.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
