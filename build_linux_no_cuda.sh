58e416b4e07d723d84c51762c392db06dcfb4029
Akhil Kumar <akhil@apralabs.com>
2022-01-21 08:19:37 +0530

    bug fix in virtual webcam... the missing reutrn statement were making release version misbehave in wierd ways. fixed now

--- build_linux_no_cuda.sh
+++ build_linux_no_cuda.sh
@@ -1,10 +1,10 @@
 mkdir -p _build
 cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=OFF  ../base 
-cmake --build . -- -j "$(($(nproc) - 2))"
+cmake --build . -- -j "$(($(nproc) - 1))"
 cd ..
 
 mkdir -p _debugbuild
 cd _debugbuild
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=OFF ../base 
-cmake --build . -- -j "$(($(nproc) - 2))"
+cmake --build . -- -j "$(($(nproc) - 1))"
--- build_linux_x64.sh
+++ build_linux_x64.sh
@@ -1,11 +1,11 @@
 mkdir -p _build
 cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../base 
-cmake --build . -- -j "$(($(nproc) - 2))"
+cmake --build . -- -j "$(($(nproc) - 1))"
 
 cd ..
 
 mkdir -p _debugbuild
 cd _debugbuild
 cmake -DCMAKE_BUILD_TYPE=Debug ../base 
-cmake --build . -- -j "$(($(nproc) - 2))"
+cmake --build . -- -j "$(($(nproc) - 1))"
