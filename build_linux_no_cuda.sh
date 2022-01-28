ada913b449f173f584fb4bfcb997b5adef67116e
Kashyap Jois <kjois@iprdgroup.com>
2022-01-28 17:13:06 +0530

    Edit linux build scripts to use vcpkg

--- build_linux_no_cuda.sh
+++ build_linux_no_cuda.sh
@@ -1,11 +1,11 @@
 CMAKE_THCOUNT=$(sh ./checkProc.sh)
 mkdir -p _build
 cd _build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=OFF  ../base 
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=OFF  ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$CMAKE_THCOUNT"
 cd ..
 
 mkdir -p _debugbuild
 cd _debugbuild
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=OFF ../base 
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=OFF ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$CMAKE_THCOUNT"
