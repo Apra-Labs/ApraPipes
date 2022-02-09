dd111bdad2e074081da24aa918bb2bf7a9f8268d
yashraj <yashrajs@apra.in>
2022-02-09 15:35:21 +0530

    Added vcpkg configure steps in build script

--- build_windows_cuda.bat
+++ build_windows_cuda.bat
@@ -1,3 +1,8 @@
+cd vcpkg
+call bootstrap-vcpkg.bat
+vcpkg.exe integrate install
+cd ..
+
 mkdir _build
 cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=ON -DENABLE_WINDOWS=ON -DENABLE_LINUX=OFF -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -A x64 ../base 
