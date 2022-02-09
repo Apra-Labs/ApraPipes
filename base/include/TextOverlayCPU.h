5f24e9e8060aa7d171a2be2612f3bb44b551e3ef
AkshayP <akshays@apra.in>
2022-02-09 16:02:58 +0530

    added text overlay cpu module with alpha blending

---
+++ base/include/TextOverlayCPU.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Module.h"
+
+class TextOverlayCPUProps : public ModuleProps
+{
+public:
+	TextOverlayCPUProps(int _frameWidth, int _frameHeight, double _alpha, std::string _text, std::string _position, bool _isDateTime, int _fontSize, std::string _fontColor, std::string _backgroundColor) : frameWidth(_frameWidth), frameHeight(_frameHeight), alpha(_alpha), text(_text), position(_position), isDateTime(_isDateTime), fontSize(_fontSize), fontColor(_fontColor), backgroundColor(_backgroundColor)
+	{
+	}
+	std::string text, fontColor, position, backgroundColor;
+	double alpha;
+	bool isDateTime;
+	int fontSize, frameWidth, frameHeight;
+
+	size_t getSerializeSize()
+	{
+		return ModuleProps::getSerializeSize() + sizeof(int) * 3 + sizeof(string) * 4 + sizeof(bool) + sizeof(double) + text.length() + fontColor.length() + position.length() + backgroundColor.length();
+	}
+
+private:
+	friend class boost::serialization::access;
+
+	template <class Archive>
+	void serialize(Archive &ar, const unsigned int version)
+	{
+		ar &boost::serialization::base_object<ModuleProps>(*this);
+		ar &text &fontSize &fontColor &isDateTime &position &frameHeight &frameWidth &alpha &backgroundColor;
+	}
+};
+
+class TextOverlayCPU : public Module
+{
+
+public:
+	TextOverlayCPU(TextOverlayCPUProps _props);
+	virtual ~TextOverlayCPU();
+	bool init();
+	bool term();
+	void setProps(TextOverlayCPUProps &props);
+	TextOverlayCPUProps getProps();
+
+protected:
+	bool process(frame_container &frames);
+	bool processSOS(frame_sp &frame);
+	bool validateInputPins();
+	bool validateOutputPins();
+	void addInputPin(framemetadata_sp &metadata, string &pinId);
+	void setProps(TextOverlayCPU);
+	bool handlePropsChange(frame_sp &frame);
+
+private:
+	void setMetadata(framemetadata_sp &metadata);
+	class Detail;
+	boost::shared_ptr<Detail> mDetail;
+};
---
+++ base/test/textoverlaycpu_tests.cpp
@@ -0,0 +1,108 @@
+#include "stdafx.h"
+#include <boost/test/unit_test.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+#include "FileReaderModule.h"
+#include "ExternalSinkModule.h"
+#include "FrameMetadata.h"
+#include "FrameMetadataFactory.h"
+#include "Frame.h"
+#include "Logger.h"
+#include "AIPExceptions.h"
+#include "MetadataHints.h"
+#include "test_utils.h"
+#include "PipeLine.h"
+#include "StatSink.h"
+#include "TextOverlayCPU.h"
+#include "StatSink.h"
+#include "FileWriterModule.h"
+#include "ImageEncoderCV.h"
+#ifdef ARM64
+BOOST_AUTO_TEST_SUITE(text_overlay_tests, *boost::unit_test::disabled())
+#else
+BOOST_AUTO_TEST_SUITE(text_overlay_tests)
+#endif
+
+BOOST_AUTO_TEST_CASE(mono)
+{
+
+    std::string text = "Apra Pipes";
+    auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/mono_1920x960.raw")));
+    auto metadata = framemetadata_sp(new RawImageMetadata(1920, 960, ImageMetadata::ImageType::MONO, CV_8UC1, 0, CV_8U, FrameMetadata::HOST, true));
+    fileReader->addOutputPin(metadata);
+
+    auto textOverlay = boost::shared_ptr<TextOverlayCPU>(new TextOverlayCPU(TextOverlayCPUProps(1920, 960, 0.5, text, "UpperRight", false, 30, "FFFFFF", "000000")));
+    fileReader->setNext(textOverlay);
+
+    auto sink = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+    textOverlay->setNext(sink);
+
+    BOOST_TEST(fileReader->init());
+    BOOST_TEST(textOverlay->init());
+    BOOST_TEST(sink->init());
+    {
+        fileReader->step();
+        textOverlay->step();
+        auto frames = sink->pop();
+        BOOST_TEST(frames.size() == 1);
+        auto outputFrame = frames.cbegin()->second;
+        BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+        Test_Utils::saveOrCompare("./data/testOutput/textOverlaymono.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(rgb)
+{
+
+    std::string text = "Apra Pipes";
+    auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/frame_1280x720_rgb.raw")));
+    auto metadata = framemetadata_sp(new RawImageMetadata(1280, 720, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
+    fileReader->addOutputPin(metadata);
+
+    auto textOverlay = boost::shared_ptr<TextOverlayCPU>(new TextOverlayCPU(TextOverlayCPUProps(1280, 720, 0.8, text, "BottomLeft", false, 30, "FFFFFF", "000000")));
+    fileReader->setNext(textOverlay);
+
+    auto sink = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+    textOverlay->setNext(sink);
+
+    BOOST_TEST(fileReader->init());
+    BOOST_TEST(textOverlay->init());
+    BOOST_TEST(sink->init());
+    {
+        fileReader->step();
+        textOverlay->step();
+        auto frames = sink->pop();
+        BOOST_TEST(frames.size() == 1);
+        auto outputFrame = frames.cbegin()->second;
+        BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+        Test_Utils::saveOrCompare("./data/testOutput/textOverlayrgb.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(bgra)
+{
+
+    std::string text = "Apra Pipes";
+    auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/overlay_1920x960_BGRA.raw")));
+    auto metadata = framemetadata_sp(new RawImageMetadata(1920, 960, ImageMetadata::ImageType::BGRA, CV_8UC4, 0, CV_8U, FrameMetadata::HOST, true));
+    fileReader->addOutputPin(metadata);
+
+    auto textOverlay = boost::shared_ptr<TextOverlayCPU>(new TextOverlayCPU(TextOverlayCPUProps(1920, 960, 1.0, text, "BottomRight", false, 30, "FFFFFF", "000000")));
+    fileReader->setNext(textOverlay);
+
+    auto sink = boost::shared_ptr<ExternalSinkModule>(new ExternalSinkModule());
+    textOverlay->setNext(sink);
+
+    BOOST_TEST(fileReader->init());
+    BOOST_TEST(textOverlay->init());
+    BOOST_TEST(sink->init());
+    {
+        fileReader->step();
+        textOverlay->step();
+        auto frames = sink->pop();
+        BOOST_TEST(frames.size() == 1);
+        auto outputFrame = frames.cbegin()->second;
+        BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE);
+        Test_Utils::saveOrCompare("./data/testOutput/textOverlayrgba.raw", const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data())), outputFrame->size(), 0);
+    }
+}
+BOOST_AUTO_TEST_SUITE_END()
