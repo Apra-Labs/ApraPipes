7b21088e8c59d0c006031aeb70148ab9b3dd75f8
Akhil Kumar <akumar@blub0x.com>
2022-02-09 20:03:14 -0500

    fixed the boost bind related compilation woarnings, fixed av_packet related compilation warnings... tested rtsp pusher, added multiple test data sets for rtsp pusher

--- base/include/FrameContainerQueue.h
+++ base/include/FrameContainerQueue.h
@@ -10,6 +10,7 @@ class FrameContainerQueue :public bounded_buffer<frame_container> {
 public:
 	FrameContainerQueue(size_t capacity);
 	virtual void push(frame_container item);
+	virtual void push_drop_oldest(frame_container item);
 	virtual frame_container pop();
 
 	virtual bool try_push(frame_container item);
