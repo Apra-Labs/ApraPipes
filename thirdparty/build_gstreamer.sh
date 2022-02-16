d25294abb41d452278d49e8efb6baca81a3dde3a
AkshayP <akshays@apra.in>
2022-02-16 16:56:51 +0530

    fix

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -2,7 +2,7 @@ cd gst-build
 ./gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
-/root/.local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
+sudo /root/.local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
 ninja -C builddir
-/root/.local/bin/meson install -C builddir
+sudo /root/.local/bin/meson install -C builddir
 cd ..
