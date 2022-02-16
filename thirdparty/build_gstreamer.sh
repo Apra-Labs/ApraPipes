dcd4698f011151594311622c465adee1456b7a88
AkshayP <akshays@apra.in>
2022-02-16 16:54:18 +0530

    changed meson location

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -2,7 +2,7 @@ cd gst-build
 ./gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
-/usr/local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
+/root/.local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
 ninja -C builddir
-/usr/local/bin/meson install -C builddir
+/root/.local/bin/meson install -C builddir
 cd ..
