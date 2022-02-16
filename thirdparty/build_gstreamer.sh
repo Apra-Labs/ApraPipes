464ed97da363f8d68409c70af0bfda3420862a85
AkshayP <akshays@apra.in>
2022-02-16 16:51:27 +0530

    using usr loca binl meson

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -2,7 +2,7 @@ cd gst-build
 ./gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
-meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
+/usr/local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
 ninja -C builddir
-meson install -C builddir
+/usr/local/bin/meson install -C builddir
 cd ..
