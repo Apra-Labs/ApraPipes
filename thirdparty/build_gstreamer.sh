2a26807b52644d1bc51919370356654a5bd46dcf
AkshayP <akshays@apra.in>
2022-02-17 17:22:50 +0530

    build gst 1.16.2

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -1,5 +1,8 @@
-cd gstreamer
-meson --prefix=${PWD}/outInstall builddir -Dgpl=enabled
+cd gst-build
+./gst-worktree.py add gst-build-1.16 origin/1.16
+cd gst-build-1.16
+git checkout 1.16.2
+sudo /root/.local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
 ninja -C builddir
-meson install -C builddir
-cd ..
+sudo /root/.local/bin/meson install -C builddir
+cd .. 
