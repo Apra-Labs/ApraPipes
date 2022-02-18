452b2c18d69b63e75278b342318f8b024a5e49bb
Akshay <akshays@apra.in>
2022-02-18 16:18:07 +0530

    added windows build script gstreamer

---
+++ thirdparty/build_gstreamer.bat
@@ -0,0 +1,8 @@
+cd gst-build
+python gst-worktree.py add gst-build-1.16 origin/1.16
+cd gst-build-1.16
+git checkout 1.16.2
+set PKG_CONFIG_PATH=C:\src\ApraPipes\vcpkg\installed\x64-windows\lib\pkgconfig
+meson --prefix=%cd%/outInstall builddir -Dpython=disabled
+meson compile -C builddir
+meson install -C builddir
