2f1ab0842a97270ffeefd93108b4235f394f1ecb
Akshay <akshays@apra.in>
2022-02-21 16:29:57 +0530

    fix3

--- build_windows.bat
+++ build_windows.bat
@@ -10,8 +10,8 @@ cmake --build .
 cd ..
 pause
 
-mkdir _debugbuild
-cd _debugbuild
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=ON -DENABLE_WINDOWS=ON -DENABLE_LINUX=OFF -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -A x64 ../base 
-cmake --build .
-pause
+@REM mkdir _debugbuild
+@REM cd _debugbuild
+@REM cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=ON -DENABLE_WINDOWS=ON -DENABLE_LINUX=OFF -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -A x64 ../base 
+@REM cmake --build .
+@REM pause
