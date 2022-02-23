7ac96d1302c9c10b912db3efe3acd2f09777272c
Kashyap Jois <kjois@iprdgroup.com>
2022-02-23 19:50:35 +0530

    CHeck gstreamer install

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -2,6 +2,7 @@ cd gst-build
 python gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
+dir
 meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
 meson compile -C builddir
 meson install -C builddir
