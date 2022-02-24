ad63743116da375020d194e53cb543a563c9d313
Akshay <akshays@apra.in>
2022-02-24 15:54:35 +0530

    locating vcvarsall

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -3,6 +3,6 @@ python gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
 set PKG_CONFIG_PATH=C:\src\ApraPipes\vcpkg\installed\x64-windows\lib\pkgconfig
-meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled --backend vs
+meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
 meson compile -C builddir
 meson install -C builddir
