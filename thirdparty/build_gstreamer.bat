bcc9034368e07dbdc2c7dfb89a2bf2a51652e46a
Akshay <akshays@apra.in>
2022-02-24 13:31:03 +0530

    fix

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -3,7 +3,6 @@ python gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
 set PKG_CONFIG_PATH=C:\src\ApraPipes\vcpkg\installed\x64-windows\lib\pkgconfig
-set CC=cl
-meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
+meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled --backend vs
 meson compile -C builddir
 meson install -C builddir
