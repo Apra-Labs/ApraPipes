3c7114d19b41575e34c29970afccb9083bff5c32
Akshay <akshays@apra.in>
2022-02-24 10:02:35 +0530

    backend vc

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -4,6 +4,6 @@ cd gst-build-1.16
 git checkout 1.16.2
 set PKG_CONFIG_PATH=C:\src\ApraPipes\vcpkg\installed\x64-windows\lib\pkgconfig
 set CC=cl
-meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
+meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled --backend vs
 meson compile -C builddir
 meson install -C builddir
