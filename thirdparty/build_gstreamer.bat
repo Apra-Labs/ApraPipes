15378556741c2c4e12cde024cb1b00bc9ca064ad
Kashyap Jois <kjois@iprdgroup.com>
2022-02-25 10:12:22 +0530

    Remove set PKG_CONFIG_PATH

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -2,7 +2,6 @@ cd gst-build
 python gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
-set PKG_CONFIG_PATH=C:\src\ApraPipes\vcpkg\installed\x64-windows\lib\pkgconfig
-meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
+meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled --backend=vs
 meson compile -C builddir
 meson install -C builddir
