022e4fc381d00919d4d97f11e60e36edf6a51c00
Kashyap Jois <kjois@iprdgroup.com>
2022-02-27 13:18:01 +0530

    use pwshl syntax for env var

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -3,7 +3,7 @@ python gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
 dir
-set VSLANG=1033
-CC=clang-cl CC_LD=link meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
-CC=clang-cl CC_LD=link meson compile -C builddir
-CC=clang-cl CC_LD=link meson install -C builddir
+$env:VSLANG=1033
+$env:CC=clang-cl $env:CC_LD=link meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
+$env:CC=clang-cl $env:CC_LD=link meson compile -C builddir
+$env:CC=clang-cl $env:CC_LD=link meson install -C builddir
