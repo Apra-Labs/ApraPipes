c6b2b10da235b501e3c554c0009a7e5e47d8fdcc
Kashyap Jois <kjois@iprdgroup.com>
2022-02-27 19:32:57 +0530

    Build in C

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -4,6 +4,6 @@ cd gst-build-1.16
 git checkout 1.16.2
 dir
 set VSLANG=1033
-meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled -Dgst-plugins-bad:openh264=disabled --backend vs
-meson compile -C builddir
-meson install -C builddir
+meson --prefix=%cd%\outInstall C:\gstreamerTemp\builddir -Dpython=disabled -Ddevtools=disabled 
+meson compile -C C:\gstreamerTemp\builddir
+meson install -C C:\gstreamerTemp\builddir
