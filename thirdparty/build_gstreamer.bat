19e75875bfd8ee014e740d589cdbafcc3e80b07f
Kashyap Jois <kjois@iprdgroup.com>
2022-02-27 20:37:12 +0530

    Build in D drive

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -4,6 +4,6 @@ cd gst-build-1.16
 git checkout 1.16.2
 dir
 set VSLANG=1033
-meson --prefix=%cd%\outInstall C:\gstreamerTemp\builddir -Dpython=disabled -Ddevtools=disabled 
-meson compile -C C:\gstreamerTemp\builddir "--vs-args=/MP"
-meson install -C C:\gstreamerTemp\builddir
+meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled 
+meson compile -C builddir "--vs-args=/MP"
+meson install -C builddir
