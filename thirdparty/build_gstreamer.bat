428dc7f6c502fb539a5ee5d3d433fe5f9fa58fcc
Kashyap Jois <kjois@iprdgroup.com>
2022-02-27 17:57:58 +0530

    Using vs backend

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -4,11 +4,6 @@ cd gst-build-1.16
 git checkout 1.16.2
 dir
 set VSLANG=1033
-set CC=clang
-set CC_LD=link
-echo "%link% %PATH%"
-set link="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\link.exe"
-echo "%VSLANG% %CC% %CC_LD% %link%"
-meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
+meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled --backend vs
 meson compile -C builddir
 meson install -C builddir
