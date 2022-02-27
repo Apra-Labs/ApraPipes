be9d317ef6c842406e47676299186d530efd7196
Kashyap Jois <kjois@iprdgroup.com>
2022-02-27 13:04:31 +0530

    Set english VSLANG

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -3,6 +3,7 @@ python gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
 dir
+set VSLANG=1033
 meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
 meson compile -C builddir
 meson install -C builddir
