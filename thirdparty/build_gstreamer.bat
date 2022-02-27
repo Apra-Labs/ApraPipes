70fafcd1e8e8dcf09ad25dbc24201958e97273f9
Kashyap Jois <kjois@iprdgroup.com>
2022-02-27 17:52:38 +0530

    Use clang

--- thirdparty/build_gstreamer.bat
+++ thirdparty/build_gstreamer.bat
@@ -4,10 +4,11 @@ cd gst-build-1.16
 git checkout 1.16.2
 dir
 set VSLANG=1033
-set CC=cl
+set CC=clang
 set CC_LD=link
+echo "%link% %PATH%"
 set link="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\link.exe"
-echo "%VSLANG% %CC% %CC_LD%"
+echo "%VSLANG% %CC% %CC_LD% %link%"
 meson --prefix=%cd%\outInstall builddir -Dpython=disabled -Ddevtools=disabled
 meson compile -C builddir
 meson install -C builddir
