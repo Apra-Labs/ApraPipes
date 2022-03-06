2eed5385e72da0a95c832943a84a07fb705bafb3
Akhil Kumar <kumaakh@users.noreply.github.com>
2022-03-05 19:07:29 -0500

    try with mason

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -2,7 +2,7 @@ cd gst-build
 ./gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
-sudo /root/.local/bin/meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
+meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
 ninja -C builddir
-sudo /root/.local/bin/meson install -C builddir
+sudo meson install -C builddir
 cd ../..
