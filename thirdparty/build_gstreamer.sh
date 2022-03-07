f6f2e7ce1bdf142d5cabe34bc221c9cbed8fb834
AkshayP <akshays@apra.in>
2022-03-07 14:31:55 +0530

    meson from pip3 only

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -1,3 +1,4 @@
+# meson from pip3 only
 cd gst-build
 ./gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
@@ -5,4 +6,4 @@ git checkout 1.16.2
 meson --prefix=${PWD}/outInstall builddir -Dpython=disabled
 ninja -C builddir
 sudo meson install -C builddir
-cd ../../..
+cd ../..
