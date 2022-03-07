eefa9e7b29ddae12e1e70e71ff09cf3fe7d52b17
AkshayP <akshays@apra.in>
2022-03-07 15:25:23 +0530

    not req

--- base/test/eventsource_tests.cpp
+++ base/test/eventsource_tests.cpp
@@ -6,7 +6,6 @@
 
 #include <boost/test/unit_test.hpp>
 #include <sw/redis++/redis++.h>
-#include "BrightnessContrastControl.h"
 #include "EventSource.h"
 #include "Logger.h"
 #include "AIPExceptions.h"
