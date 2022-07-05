95195c15459887ba45d3883706ca935b510e82f9
Vinayak Y B <89510107+Vinayak-YB@users.noreply.github.com>
2022-07-05 19:39:07 +0530

    Fix for Filewriter Append Issues (#114)
    * Fix for Filewriter Append Issues
    
    * Fix for Append File
    
    * Updated Test Cases
    
    * FileCleaner updated

--- base/test/filewritermodule_tests.cpp
+++ base/test/filewritermodule_tests.cpp
@@ -9,11 +9,29 @@
 #include "Logger.h"
 #include "AIPExceptions.h"
 #include "test_utils.h"
-
+#include <fstream>
+#include <vector>
 BOOST_AUTO_TEST_SUITE(filewritermodule_tests)
 
-BOOST_AUTO_TEST_CASE(basic, * boost::unit_test::disabled())
+struct FileCleaner {
+	FileCleaner(std::vector<std::string> paths) {
+		pathsOfFiles = paths;
+	};
+	~FileCleaner() {
+		for (int i = 0; i < pathsOfFiles.size(); i++) {
+			boost::filesystem::path filePath(pathsOfFiles[i]);
+			if (boost::filesystem::exists(filePath))
+			{
+				boost::filesystem::remove(filePath);
+			}
+		}
+	};
+	std::vector<std::string> pathsOfFiles;
+};
+BOOST_AUTO_TEST_CASE(basic)
 {
+	std::vector<std::string> Files = { "./data/testOutput/fileWriterModuleFrame_0000.jpg" , "./data/testOutput/fileWriterModuleFrame_0001.jpg", "./data/testOutput/fileWriterModuleFrame_0002.jpg", "./data/testOutput/fileWriterModuleFrame_0003.jpg" };
+	FileCleaner f(Files);
 	const uint8_t* pReadData = nullptr;
 	unsigned int readDataSize = 0U;
 	BOOST_TEST(Test_Utils::readFile("./data/mono.jpg", pReadData, readDataSize));
@@ -50,6 +68,79 @@ BOOST_AUTO_TEST_CASE(basic, * boost::unit_test::disabled())
 	}
 
 	delete[] pReadData;
+}
+
+BOOST_AUTO_TEST_CASE(append)
+{
+	std::vector<std::string> Files = { "./data/testOutput/fileWriterModuleSample.txt" };
+	FileCleaner f(Files);
+	unsigned int readDataSize = 0U;
+	ofstream myFile("./data/testOutput/fileWriterModuleSample.txt");
+	myFile << "Foo";
+	myFile.close(); 
+	auto m1 = boost::shared_ptr<ExternalSourceModule>(new ExternalSourceModule());
+	auto metadata = framemetadata_sp(new FrameMetadata(FrameMetadata::ENCODED_IMAGE));
+	auto pinId = m1->addOutputPin(metadata);
+	auto m2 = boost::shared_ptr<FileWriterModule>(new FileWriterModule(FileWriterModuleProps("./data/testOutput/fileWriterModuleSample.txt", true)));
+	m1->setNext(m2);
+
+	BOOST_TEST(m1->init());
+	BOOST_TEST(m2->init());
+	const char *stringToAppend = "bar";
+	readDataSize = std::strlen(stringToAppend);
+	auto frame = m1->makeFrame(readDataSize, pinId);
+	memcpy(frame->data(), stringToAppend, readDataSize);
+
+	frame_container frames;
+	frames.insert(make_pair(pinId, frame));
+	m1->send(frames);
+	m2->step();
+	string text;
+	ifstream readFile("./data/testOutput/fileWriterModuleSample.txt");
+	std::getline(readFile, text);
+	readFile.close();
+	BOOST_TEST(text == "Foobar"); 
+}
+
+BOOST_AUTO_TEST_CASE(appendTestPattern)
+{
+	std::vector<std::string> Files = { "./data/testOutput/fileWriterModuleSample_0000.txt" };
+	FileCleaner f(Files);
+	unsigned int readDataSize = 0U;
+	
+	auto m1 = boost::shared_ptr<ExternalSourceModule>(new ExternalSourceModule());
+	auto metadata = framemetadata_sp(new FrameMetadata(FrameMetadata::ENCODED_IMAGE));
+	auto pinId = m1->addOutputPin(metadata);
+	auto m2 = boost::shared_ptr<FileWriterModule>(new FileWriterModule(FileWriterModuleProps("./data/testOutput/fileWriterModuleSample_????.txt", true)));
+	m1->setNext(m2);
+
+	BOOST_TEST(m1->init());
+	BOOST_TEST(m2->init());
+	const char *stringToAppend = "Foo";
+	readDataSize = strlen(stringToAppend);
+	auto frame = m1->makeFrame(readDataSize, pinId);
+	memcpy(frame->data(), stringToAppend, readDataSize);
+
+	frame_container frames;
+	frames.insert(make_pair(pinId, frame));
+	m1->send(frames);
+	m2->step();
+
+	string text;
+	ifstream readFile("./data/testOutput/fileWriterModuleSample_0000.txt");
+	std::getline(readFile, text);
+	readFile.close();
+	BOOST_TEST(text == "Foo");
+
+	stringToAppend = "bar";
+	readDataSize = strlen(stringToAppend);
+	memcpy(frame->data(), stringToAppend, readDataSize);
+	m1->send(frames);
+	m2->step();
+	readFile.open("./data/testOutput/fileWriterModuleSample_0000.txt");
+	std::getline(readFile, text);
+	readFile.close();
+	BOOST_TEST(text == "Foobar");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
