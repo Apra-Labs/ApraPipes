4b3338b9a069aaac722a10cb5e8855e53ccce9b5
Akhil Kumar <akhil.kumar@gmail.com>
2022-09-02 00:34:27 +0530

    some tiny script fixes

--- build_windows_cuda.bat
+++ build_windows_cuda.bat
@@ -9,7 +9,7 @@ cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=ON -DENABLE_WINDOWS=ON -DENABLE_LINUX=OFF -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -A x64 ../base 
 cmake --build .
 cd ..
-
+rem goto :EOF
 mkdir _debugbuild
 cd _debugbuild
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=ON -DENABLE_WINDOWS=ON -DENABLE_LINUX=OFF -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -A x64 ../base 
