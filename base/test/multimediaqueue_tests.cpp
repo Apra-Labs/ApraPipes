82a5d005dee53d4847628dc366d32e55e482df95
Vinayak-YB <vinayak.bhustali@gmail.com>
2022-09-16 16:32:57 +0530

    TestCases refactored

--- base/test/multimediaqueue_tests.cpp
+++ base/test/multimediaqueue_tests.cpp
@@ -54,9 +54,8 @@ protected:
 
 //  # BOOST FIXTURE TEST CASES 
 
-BOOST_AUTO_TEST_CASE(export_state)
+int testQueue(uint32_t queuelength, uint16_t tolerance, bool isMapInTime, int i1, int i2, uint64_t startTime, uint64_t endTime)
 {
-    //In this case both the timestamps (query startTime and query endTime) are in the queue and we pass all the frames requested.
     std::string inFolderPath = "./data/Raw_YUV420_640x360";
     auto fileReaderProps = FileReaderModuleProps(inFolderPath, 0, -1, 4 * 1024 * 1024);
     fileReaderProps.fps = 1;
@@ -65,7 +64,7 @@ BOOST_AUTO_TEST_CASE(export_state)
     auto metadata = framemetadata_sp(new RawImageMetadata(640, 360, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
     auto pinId = fileReader->addOutputPin(metadata);
 
-    auto multiQueue = boost::shared_ptr<MultimediaQueue>(new MultimediaQueue(MultimediaQueueProps(10, 5, false))); // 
+    auto multiQueue = boost::shared_ptr<MultimediaQueue>(new MultimediaQueue(MultimediaQueueProps(queuelength, tolerance, isMapInTime))); // 
     fileReader->setNext(multiQueue);
     auto sink = boost::shared_ptr<SinkModule>(new SinkModule(SinkModuleProps())); //
 
@@ -75,194 +74,91 @@ BOOST_AUTO_TEST_CASE(export_state)
     BOOST_TEST(multiQueue->init());
     BOOST_TEST(sink->init());
     auto sinkQueue = sink->getQue();
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < i1; i++)
     {
         fileReader->step();
         multiQueue->step();
     }
     unsigned __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-    uint64_t startTime = now - 9000;
-    startTime = (startTime / 1000) * 1000;
-    uint64_t endTime = now - 4000;
-    endTime = (endTime / 1000) * 1000;
     multiQueue->allowFrames(startTime, endTime);
     multiQueue->step();
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < i2; i++)
     {
         fileReader->step();
         multiQueue->step();
     }
-    BOOST_TEST(sinkQueue->size() == 5);
+    return sinkQueue->size();
+}
+
+BOOST_AUTO_TEST_CASE(export_state)
+{
+    //In this case both the timestamps (query startTime and query endTime) are in the queue and we pass all the frames requested.
+
+    unsigned __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    uint64_t startTime = now + 19000;
+    startTime = (startTime / 1000) * 1000;
+    uint64_t endTime = now + 24000;
+    endTime = (endTime / 1000) * 1000;
+ 
+    int queueSize = testQueue(10000,5000,true, 20, 5, startTime, endTime);
+    BOOST_TEST(queueSize  == 5);
 }
 
 BOOST_AUTO_TEST_CASE(idle_state)
 {
     //In this case both the timestamps (query startTime and endTime) are in the past of the oldest timestamp of queue so state is Idle all the time
-    std::string inFolderPath = "./data/Raw_YUV420_640x360";
-    auto fileReaderProps = FileReaderModuleProps(inFolderPath, 0, -1, 4 * 1024 * 1024);
-    fileReaderProps.fps = 1;
-    fileReaderProps.readLoop = true;
-    auto fileReader = boost::shared_ptr<Module>(new FileReaderModule(fileReaderProps));
-    auto metadata = framemetadata_sp(new RawImageMetadata(640, 360, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
-    auto pinId = fileReader->addOutputPin(metadata);
 
-    auto multiQueue = boost::shared_ptr<MultimediaQueue>(new MultimediaQueue(MultimediaQueueProps(10000, true)));
-    fileReader->setNext(multiQueue);
-    auto sink = boost::shared_ptr<SinkModule>(new SinkModule(SinkModuleProps()));
-
-    multiQueue->setNext(sink);
-
-    BOOST_TEST(fileReader->init());
-    BOOST_TEST(multiQueue->init());
-    BOOST_TEST(sink->init());
-    auto sinkQueue = sink->getQue();
-    for (int i = 0; i < 20; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
     unsigned __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    uint64_t startTime = now - 30000;
+    uint64_t startTime = now - 5000;
     startTime = (startTime / 1000) * 1000;
-    uint64_t endTime = now - 25000;
+    uint64_t endTime = now;
     endTime = (endTime / 1000) * 1000;
-    multiQueue->allowFrames(startTime, endTime);
-    multiQueue->step();
 
-    for (int i = 0; i < 5; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
-    BOOST_TEST(sinkQueue->size() == 0);
+    int queueSize = testQueue(10000, 5000, true, 20, 5, startTime, endTime);
+    BOOST_TEST(queueSize == 0);
 }
 
 BOOST_AUTO_TEST_CASE(wait_state)
 {
     //In this case both the timestamps (query startTime and endTime) are in the future of the latest timestamp of queue so state is Waiting all the time
-    std::string inFolderPath = "./data/Raw_YUV420_640x360";
-    auto fileReaderProps = FileReaderModuleProps(inFolderPath, 0, -1, 4 * 1024 * 1024);
-    fileReaderProps.fps = 1;
-    fileReaderProps.readLoop = true;
-    auto fileReader = boost::shared_ptr<Module>(new FileReaderModule(fileReaderProps));
-    auto metadata = framemetadata_sp(new RawImageMetadata(640, 360, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
-    auto pinId = fileReader->addOutputPin(metadata);
-
-    auto multiQueue = boost::shared_ptr<MultimediaQueue>(new MultimediaQueue(MultimediaQueueProps(10000, true)));
-    fileReader->setNext(multiQueue);
-    auto sink = boost::shared_ptr<SinkModule>(new SinkModule(SinkModuleProps()));
-
-    multiQueue->setNext(sink);
-
-    BOOST_TEST(fileReader->init());
-    BOOST_TEST(multiQueue->init());
-    BOOST_TEST(sink->init());
-    auto sinkQueue = sink->getQue();
-    for (int i = 0; i < 20; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
+   
     unsigned __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    uint64_t startTime = now + 5000;
+    uint64_t startTime = now + 33000;
     startTime = (startTime / 1000) * 1000;
-    uint64_t endTime = now + 10000;
+    uint64_t endTime = now + 38000;
     endTime = (endTime / 1000) * 1000;
-    multiQueue->allowFrames(startTime, endTime);
-    multiQueue->step();
-
-    for (int i = 0; i < 5; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
-    BOOST_TEST(sinkQueue->size() == 0);
+    
+    int queueSize = testQueue(10000, 5000, true, 20, 5, startTime, endTime);
+    BOOST_TEST(queueSize == 0);
 }
 
 BOOST_AUTO_TEST_CASE(wait_to_export_state)
 {
     //In this case initially we are in wait state then go to export after sometime.
-    std::string inFolderPath = "./data/Raw_YUV420_640x360";
-    auto fileReaderProps = FileReaderModuleProps(inFolderPath, 0, -1, 4 * 1024 * 1024);
-    fileReaderProps.fps = 1;
-    fileReaderProps.readLoop = true;
-    auto fileReader = boost::shared_ptr<Module>(new FileReaderModule(fileReaderProps));
-    auto metadata = framemetadata_sp(new RawImageMetadata(640, 360, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
-    auto pinId = fileReader->addOutputPin(metadata);
-
-    auto multiQueue = boost::shared_ptr<MultimediaQueue>(new MultimediaQueue(MultimediaQueueProps(10000, true)));
-    fileReader->setNext(multiQueue);
-    auto sink = boost::shared_ptr<SinkModule>(new SinkModule(SinkModuleProps()));
-
-    multiQueue->setNext(sink);
-
-    BOOST_TEST(fileReader->init());
-    BOOST_TEST(multiQueue->init());
-    BOOST_TEST(sink->init());
-    auto sinkQueue = sink->getQue();
-    for (int i = 0; i < 20; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
+   
     unsigned __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    uint64_t startTime = now + 3000;
+    uint64_t startTime = now + 24000;
     startTime = (startTime / 1000) * 1000;
-    uint64_t endTime = now + 11000;
+    uint64_t endTime = now + 33000;
     endTime = (endTime / 1000) * 1000;
-    multiQueue->allowFrames(startTime, endTime);
-    multiQueue->step();
-
-    for (int i = 0; i < 6; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
-    BOOST_TEST(sinkQueue->size() == 3);
+    
+    int queueSize = testQueue(10000, 5000, true, 20, 6, startTime, endTime);
+    BOOST_TEST(queueSize == 2);
 }
 
 BOOST_AUTO_TEST_CASE(future_export)
 {
     //In this case the timestamp of startTime is in the queue while endTime is in future so we start with export and continue to stay in export as frames are passed.
-    std::string inFolderPath = "./data/Raw_YUV420_640x360";
-    auto fileReaderProps = FileReaderModuleProps(inFolderPath, 0, -1, 4 * 1024 * 1024);
-    fileReaderProps.fps = 1;
-    fileReaderProps.readLoop = true;
-    auto fileReader = boost::shared_ptr<Module>(new FileReaderModule(fileReaderProps));
-    auto metadata = framemetadata_sp(new RawImageMetadata(640, 360, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
-    auto pinId = fileReader->addOutputPin(metadata);
-
-    auto multiQueue = boost::shared_ptr<MultimediaQueue>(new MultimediaQueue(MultimediaQueueProps(10000, true)));
-    fileReader->setNext(multiQueue);
-    auto sink = boost::shared_ptr<SinkModule>(new SinkModule(SinkModuleProps()));
-
-    multiQueue->setNext(sink);
-
-    BOOST_TEST(fileReader->init());
-    BOOST_TEST(multiQueue->init());
-    BOOST_TEST(sink->init());
-    auto sinkQueue = sink->getQue();
-    for (int i = 0; i < 20; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
     unsigned __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    uint64_t startTime = now - 3000;
+    uint64_t startTime = now + 19000;
     startTime = (startTime / 1000) * 1000;
-    uint64_t endTime = now + 5000;
+    uint64_t endTime = now + 27000;
     endTime = (endTime / 1000) * 1000;
-    multiQueue->allowFrames(startTime, endTime);
-    multiQueue->step();
-
-    for (int i = 0; i < 15; i++)
-    {
-        fileReader->step();
-        multiQueue->step();
-    }
-    BOOST_TEST(sinkQueue->size() == 8);
+    
+    int queueSize = testQueue(10000, 5000, true, 20, 15, startTime, endTime);
+    BOOST_TEST(queueSize == 8);
 }
 
 BOOST_AUTO_TEST_CASE(nextQueue_full)
