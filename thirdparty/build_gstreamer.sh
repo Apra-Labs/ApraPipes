1cc23de8437106918d51707f42cd81314c939810
Akhil Kumar <akhil.kumar@gmail.com>
2022-09-29 12:44:24 +0530

    try7

--- thirdparty/build_gstreamer.sh
+++ thirdparty/build_gstreamer.sh
@@ -8,6 +8,8 @@ rm -rf gst-build-1.16 || true
 git worktree prune
 git worktree list
 
+dos2unix ./gst-worktree.py
+
 ./gst-worktree.py add gst-build-1.16 origin/1.16
 cd gst-build-1.16
 git checkout 1.16.2
