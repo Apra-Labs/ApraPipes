0dbc6947ff75b62a2fed4916078505c26c5b8287
Vinayak-YB <vinayak.bhustali@gmail.com>
2022-10-04 11:15:01 +0530

    TestCases refactored for better readability

--- base/test/valveModule_tests.cpp
+++ base/test/valveModule_tests.cpp
@@ -79,7 +79,7 @@ BOOST_AUTO_TEST_CASE(basic)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 4);
+    BOOST_TEST(sinkQue->size() == 4,"Four frames are passed to Sink");
 
     //The props are changed by passing 2 in allowFrames()
     valve->allowFrames(2);
@@ -92,7 +92,7 @@ BOOST_AUTO_TEST_CASE(basic)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 6);
+    BOOST_TEST(sinkQue->size() == 6,"Additional two frames are passed to Sink, total frames 6");
 
     //We are sending 4 frames again with enable false
     for (int i = 0; i < 4; i++)
@@ -101,7 +101,7 @@ BOOST_AUTO_TEST_CASE(basic)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 6);
+    BOOST_TEST(sinkQue->size() == 6,"Enable false, so no frames were passed to sink, total frames remains 6");
 
     // The props are changed by passing 0 in allowFrames()
     valve->allowFrames(0);
@@ -120,10 +120,10 @@ BOOST_AUTO_TEST_CASE(multiple_pins)
     valve module - sieve = disabled
     2 succesive modules of valve */
     auto source = boost::shared_ptr<ExternalSourceModule>(new ExternalSourceModule());
-    auto metadata = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::GENERAL));
-    auto metadata1 = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::RAW_IMAGE));
-    auto pinId = source->addOutputPin(metadata);
-    auto pinId1 = source->addOutputPin(metadata1);
+    auto metadataGeneral = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::GENERAL));
+    auto metadataRaw = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::RAW_IMAGE));
+    auto pinIdGeneral = source->addOutputPin(metadataGeneral);
+    auto pinIdRaw = source->addOutputPin(metadataRaw);
 
     auto valve = boost::shared_ptr<ValveModule>(new ValveModule(ValveModuleProps(2)));
     // always use disable sieve with valve module 
@@ -143,19 +143,19 @@ BOOST_AUTO_TEST_CASE(multiple_pins)
     auto sink1Que = sink1->getQue();
     auto sink2Que = sink2->getQue();
 
-    auto frame = source->makeFrame(1023, pinId);
+    auto frame = source->makeFrame(1023, pinIdGeneral);
     frame_container frames;
-    frames.insert(make_pair(pinId, frame));
-    auto frame1 = source->makeFrame(1023, pinId1);
-    frames.insert(make_pair(pinId1, frame1));
+    frames.insert(make_pair(pinIdGeneral, frame));
+    auto frame1 = source->makeFrame(1023, pinIdRaw);
+    frames.insert(make_pair(pinIdRaw, frame1));
     for (int i = 0; i < 4; i++)
     {
         source->send(frames);
         valve->step();
     }
 
-    BOOST_TEST(sink1Que->size() == 0);
-    BOOST_TEST(sink2Que->size() == 0);
+    BOOST_TEST(sink1Que->size() == 0,"Enable false so no frames are passed to sink1");
+    BOOST_TEST(sink2Que->size() == 0,"Enable false so no frames are passed to sink2");
 
     valve->allowFrames(2);
     valve->step();
@@ -166,8 +166,8 @@ BOOST_AUTO_TEST_CASE(multiple_pins)
         valve->step();
     }
 
-    BOOST_TEST(sink1Que->size() == 2);
-    BOOST_TEST(sink2Que->size() == 2);
+    BOOST_TEST(sink1Que->size() == 2,"Two frames are passed to sink1");
+    BOOST_TEST(sink2Que->size() == 2,"Two frames are passed to sink2");
 
     // check the number of frames in the frame container of the sinks
     while (sink1Que->size() != 0)
@@ -189,8 +189,8 @@ BOOST_AUTO_TEST_CASE(multiple_pins)
                 flagRaw = true;
             }
         }
-        BOOST_TEST(flagGen);
-        BOOST_TEST(flagRaw);
+        BOOST_TEST(flagGen,"Sink1 has frame of type General");
+        BOOST_TEST(flagRaw,"Sink1 has frame of type Raw");
     }
 
     while (sink2Que->size() != 0)
@@ -212,8 +212,8 @@ BOOST_AUTO_TEST_CASE(multiple_pins)
                 flagRaw = true;
             }
         }
-        BOOST_TEST(flagGen);
-        BOOST_TEST(flagRaw);
+        BOOST_TEST(flagGen, "Sink2 has frame of type General");
+        BOOST_TEST(flagRaw, "Sink2 has frame of type General");
     }
 }
 
@@ -249,7 +249,7 @@ BOOST_AUTO_TEST_CASE(getSetProps)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 2);
+    BOOST_TEST(sinkQue->size() == 2,"Two frames are passed due to props-2");
 
     //We are resetting the moduleprops to 4 and enable true 
     // rvw - getProps - change the value - sending in setProps
@@ -266,7 +266,7 @@ BOOST_AUTO_TEST_CASE(getSetProps)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 6);
+    BOOST_TEST(sinkQue->size() == 6,"Four frames are passed after changing props-4, total frames 6");
 
     //We are sending 4 frames again with enable false
     for (int i = 0; i < 4; i++)
@@ -275,7 +275,7 @@ BOOST_AUTO_TEST_CASE(getSetProps)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 6);
+    BOOST_TEST(sinkQue->size() == 6,"Zero frames are passed as props-0, total frames remains 6");
 
 }
 
@@ -307,7 +307,7 @@ BOOST_AUTO_TEST_CASE(start_open)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 4);
+    BOOST_TEST(sinkQue->size() == 4,"Props=-1 so all frames are passed through Valve");
 
     //Closing the valve module
     valve->allowFrames(0);
@@ -319,17 +319,17 @@ BOOST_AUTO_TEST_CASE(start_open)
         valve->step();
     }
 
-    BOOST_TEST(sinkQue->size() == 4);
+    BOOST_TEST(sinkQue->size() == 4,"Props=0 so no additional frames are passed, total frames remains 4");
 
 }
 
 BOOST_AUTO_TEST_CASE(valve_relay)
 {
     auto source = boost::shared_ptr<ExternalSourceModule>(new ExternalSourceModule());
-    auto metadata = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::GENERAL));
-    auto metadata1 = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::RAW_IMAGE));
-    auto pinId = source->addOutputPin(metadata);
-    auto pinId1 = source->addOutputPin(metadata1);
+    auto metadataGeneral = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::GENERAL));
+    auto metadataRaw = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::RAW_IMAGE));
+    auto pinIdGeneral = source->addOutputPin(metadataGeneral);
+    auto pinIdRaw = source->addOutputPin(metadataRaw);
 
     auto valve = boost::shared_ptr<ValveModule>(new ValveModule(ValveModuleProps(2)));
     source->setNext(valve);
@@ -348,11 +348,11 @@ BOOST_AUTO_TEST_CASE(valve_relay)
     auto sink1Que = sink1->getQue();
     auto sink2Que = sink2->getQue();
 
-    auto frame = source->makeFrame(1023, pinId);
+    auto frame = source->makeFrame(1023, pinIdGeneral);
     frame_container frames;
-    frames.insert(make_pair(pinId, frame));
-    auto frame1 = source->makeFrame(1023, pinId1);
-    frames.insert(make_pair(pinId1, frame1));
+    frames.insert(make_pair(pinIdGeneral, frame));
+    auto frame1 = source->makeFrame(1023, pinIdRaw);
+    frames.insert(make_pair(pinIdRaw, frame1));
     valve->allowFrames(2);
     valve->step();
     for (int i = 0; i < 4; i++)
@@ -361,8 +361,8 @@ BOOST_AUTO_TEST_CASE(valve_relay)
         valve->step();
     }
 
-    BOOST_TEST(sink1Que->size() == 0);
-    BOOST_TEST(sink2Que->size() == 2);
+    BOOST_TEST(sink1Que->size() == 0,"No frames as Sink1 is closed");
+    BOOST_TEST(sink2Que->size() == 2,"Two frames are passed to Sink2 which is open");
 
     //Sink1 which was closed is opened using relay and Sink2 is closed using relay
     valve->relay(sink1, true);
@@ -378,8 +378,8 @@ BOOST_AUTO_TEST_CASE(valve_relay)
         valve->step();
     }
 
-    BOOST_TEST(sink1Que->size() == 2);
-    BOOST_TEST(sink2Que->size() == 2);
+    BOOST_TEST(sink1Que->size() == 2, "Two frames are passed to Sink1 which is opened");
+    BOOST_TEST(sink2Que->size() == 2, "No frames as Sink2 is now closed using relay, total frames remain 2");
 
     //Now both sinks are closed and no frames will pass to them
     valve->relay(sink1, false);
@@ -392,17 +392,17 @@ BOOST_AUTO_TEST_CASE(valve_relay)
         valve->step();
     }
 
-    BOOST_TEST(sink1Que->size() == 2);
-    BOOST_TEST(sink2Que->size() == 2);
+    BOOST_TEST(sink1Que->size() == 2, "No frames as Sink1 is now closed using relay, total frames remain 2");
+    BOOST_TEST(sink2Que->size() == 2, "No frames as Sink2 is still closed using relay, total frames remain 2");
 }
 
 BOOST_AUTO_TEST_CASE(multiType_frames)
 {
     auto source = boost::shared_ptr<ExternalSourceModule>(new ExternalSourceModule());
-    auto metadata = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::GENERAL));
-    auto metadata1 = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::RAW_IMAGE));
-    auto pinId = source->addOutputPin(metadata);
-    auto pinId1 = source->addOutputPin(metadata1);
+    auto metadataGeneral = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::GENERAL));
+    auto metadataRaw = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::RAW_IMAGE));
+    auto pinIdGeneral = source->addOutputPin(metadataGeneral);
+    auto pinIdRaw = source->addOutputPin(metadataRaw);
 
     auto valve = boost::shared_ptr<ValveModule>(new ValveModule(ValveModuleProps(4)));
     source->setNext(valve);
@@ -414,19 +414,19 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
     BOOST_TEST(valve->init());
     BOOST_TEST(sink->init());
 
-    auto frame = source->makeFrame(1023, pinId);
+    auto frame = source->makeFrame(1023, pinIdGeneral);
     frame_container bothframes;
-    bothframes.insert(make_pair(pinId, frame));
-    auto frame1 = source->makeFrame(1023, pinId1);
-    bothframes.insert(make_pair(pinId1, frame1));
+    bothframes.insert(make_pair(pinIdGeneral, frame));
+    auto frame1 = source->makeFrame(1023, pinIdRaw);
+    bothframes.insert(make_pair(pinIdRaw, frame1));
 
-    auto gframe = source->makeFrame(1023, pinId);
+    auto gframe = source->makeFrame(1023, pinIdGeneral);
     frame_container generalframes;
-    generalframes.insert(make_pair(pinId, gframe));
+    generalframes.insert(make_pair(pinIdGeneral, gframe));
 
-    auto rframe = source->makeFrame(1023, pinId1);
+    auto rframe = source->makeFrame(1023, pinIdRaw);
     frame_container rawframes;
-    rawframes.insert(make_pair(pinId1, rframe));
+    rawframes.insert(make_pair(pinIdRaw, rframe));
 
     valve->allowFrames(4);
     valve->step();
@@ -450,8 +450,8 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
             framesRaw++;
         }
     }
-    BOOST_TEST(framesGen == 1);
-    BOOST_TEST(framesRaw == 0);
+    BOOST_TEST(framesGen == 1,"First frame container contains one frame of type General");
+    BOOST_TEST(framesRaw == 0,"First frame container contains no frame of type Raw");
 
     source->send(generalframes);
     valve->step();
@@ -468,8 +468,8 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
             framesRaw++;
         }
     }
-    BOOST_TEST(framesGen == 2);
-    BOOST_TEST(framesRaw == 0);
+    BOOST_TEST(framesGen == 2,"Second frame container contains one frame of type General, total gen frames = 2");
+    BOOST_TEST(framesRaw == 0,"Second frame container contains no frame of type Raw, total raw frames = 0");
 
     source->send(bothframes);
     valve->step();
@@ -486,8 +486,8 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
             framesRaw++;
         }
 	}
-    BOOST_TEST(framesGen == 3);
-    BOOST_TEST(framesRaw == 1);
+    BOOST_TEST(framesGen == 3,"Third frame container contains one frame of type General, total gen frames = 3");
+    BOOST_TEST(framesRaw == 1, "Third frame container contains one frame of type Raw, total raw frames = 1");
 
     source->send(bothframes);
     valve->step();
@@ -504,8 +504,8 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
             framesRaw++;
         }
     }
-    BOOST_TEST(framesGen == 4);
-    BOOST_TEST(framesRaw == 2);
+    BOOST_TEST(framesGen == 4,"Fourth frame container contains one frame of type General, total gen frames = 4");
+    BOOST_TEST(framesRaw == 2,"Fourth frame container contains one frame of type Raw, total raw frames = 2");
 
     source->send(rawframes);
     valve->step();
@@ -522,8 +522,8 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
             framesRaw++;
         }
     }
-    BOOST_TEST(framesGen == 4);
-    BOOST_TEST(framesRaw == 3);
+    BOOST_TEST(framesGen == 4, "Fifth frame container contains no frame of type General, total gen frames = 4");
+    BOOST_TEST(framesRaw == 3, "Fifth frame container contains one frame of type Raw, total raw frames = 3");
 
     source->send(rawframes);
     valve->step();
@@ -540,13 +540,13 @@ BOOST_AUTO_TEST_CASE(multiType_frames)
             framesRaw++;
         }
     }
-    BOOST_TEST(framesGen == 4);
-    BOOST_TEST(framesRaw == 4);
+    BOOST_TEST(framesGen == 4, "Sixth frame container contains no frame of type General, total gen frames = 4");
+    BOOST_TEST(framesRaw == 4, "Sixth frame container contains one frame of type Raw, total raw frames = 4");
 
     source->send(bothframes);
     valve->step();
 
-    BOOST_TEST(sinkQue->size() == 0);
+    BOOST_TEST(sinkQue->size() == 0,"All frame containers are popped for assertion so zero");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
