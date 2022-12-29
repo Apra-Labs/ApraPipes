a9c5352debf02d59bb5fe85dc0cea3a5b85e4c8a
zaki <zakik@apra.in>
2022-12-29 19:35:35 +0530

    Added a new test in v4l2 decoder

--- base/test/h264decoderv4l2_tests.cpp
+++ base/test/h264decoderv4l2_tests.cpp
@@ -8,8 +8,50 @@
 #include "H264Metadata.h"
 #include "H264DecoderV4L2Helper.h"
 #include "EglRenderer.h"
+#include "Mp4ReaderSource.h"
 
 BOOST_AUTO_TEST_SUITE(h264decoderv4l2codec_tests)
+
+BOOST_AUTO_TEST_CASE(Mp4Reader_Decoder_EglReenderer,*boost::unit_test::disabled())
+{
+	Logger::setLogLevel("info");
+
+	// metadata is known
+	std::string videoPath = "./data/Mp4_videos/h264_video/20221010/0012/1668063524439.mp4";
+	auto mp4ReaderProps = Mp4ReaderSourceProps(videoPath, false);
+	auto mp4Reader = boost::shared_ptr<Mp4ReaderSource>(new Mp4ReaderSource(mp4ReaderProps));
+	auto h264ImageMetadata = framemetadata_sp(new H264Metadata(0, 0));
+	mp4Reader->addOutPutPin(h264ImageMetadata);
+
+	auto mp4Metadata = framemetadata_sp(new Mp4VideoMetadata("v_1"));
+	mp4Reader->addOutPutPin(mp4Metadata);
+
+	auto Decoder = boost::shared_ptr<Module>(new H264Decoder(H264DecoderProps()));
+	std::vector<std::string> mImagePin;
+	mImagePin = mp4Reader->getAllOutputPinsByType(FrameMetadata::FrameType::H264_DATA);
+	mp4Reader->setNext(Decoder, mImagePin);
+
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0, 0)));
+	Decoder->setNext(sink);
+
+	boost::shared_ptr<PipeLine> p;
+	p = boost::shared_ptr<PipeLine>(new PipeLine("test"));
+	p->appendModule(mp4Reader);
+
+	if (!p->init())
+	{
+		throw AIPException(AIP_FATAL, "Engine Pipeline init failed. Check IPEngine Logs for more details.");
+	}
+
+	p->run_all_threaded();
+
+	Test_Utils::sleep_for_seconds(30);
+
+	p->stop();
+	p->term();
+	p->wait_for_all();
+	p.reset();
+}
 
 BOOST_AUTO_TEST_CASE(h264_to_yuv420_extSink,*boost::unit_test::disabled())
 {
