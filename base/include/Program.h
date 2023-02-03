1518d53b6602520dfd38d01e9b038cd237d63345
Vinayak-YB <vinayak.bhustali@gmail.com>
2023-02-03 23:06:55 +0530

    Added GtkGL Renderer Module

---
+++ base/include/Program.h
@@ -0,0 +1,22 @@
+#include <GL/gl.h>
+
+void initProgram (void);
+void programs_init (void);
+void program_cube_use (void);
+void program_bkgd_use (void);
+
+enum LocBkgd {
+	LOC_BKGD_VERTEX,
+	LOC_BKGD_TEXTURE,
+};
+
+enum LocCube {
+	LOC_CUBE_VIEW,
+	LOC_CUBE_MODEL,
+	LOC_CUBE_VERTEX,
+	LOC_CUBE_VCOLOR,
+	LOC_CUBE_NORMAL,
+};
+
+GLint program_bkgd_loc (const enum LocBkgd);
+GLint program_cube_loc (const enum LocCube);
