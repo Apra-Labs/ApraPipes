3cdf40d63961b6a61dd2356ab2ae181a3722da01
venkat0907 <venkatnallamothu0907@gmail.com>
2023-02-21 10:31:21 +0530

    corrected _buildNoCuda to _build

--- build_linux_no_cuda.sh
+++ build_linux_no_cuda.sh
@@ -6,13 +6,13 @@ cd vcpkg
 cd ..
 
 CMAKE_THCOUNT=$(sh ./checkProc.sh)
-mkdir -p _buildNoCuda
-cd _buildNoCuda
+mkdir -p _build
+cd _build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=OFF  ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$CMAKE_THCOUNT"
 cd ..
 
-mkdir -p _debugbuildNoCuda
-cd _debugbuildNoCuda
+mkdir -p _debugbuild
+cd _debugbuild
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=OFF ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$CMAKE_THCOUNT"
