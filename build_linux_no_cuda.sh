e9ab29183b9e0713df5ad240a45ac10440e1b51a
venkat0907 <venkatnallamothu0907@gmail.com>
2023-02-21 10:08:06 +0530

    fixed linux_nocuda issue

--- build_linux_no_cuda.sh
+++ build_linux_no_cuda.sh
@@ -1,12 +1,18 @@
-cp base/vcpkg.nocuda.json base/vcpkg.json
+chmod +x base/fix-vcpkg-json.sh
+./base/fix-vcpkg-json.sh true fasle false
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
+cd ..
+
 CMAKE_THCOUNT=$(sh ./checkProc.sh)
-mkdir -p _build
-cd _build
+mkdir -p _buildNoCuda
+cd _buildNoCuda
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_CUDA=OFF  ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$CMAKE_THCOUNT"
 cd ..
 
-mkdir -p _debugbuild
-cd _debugbuild
+mkdir -p _debugbuildNoCuda
+cd _debugbuildNoCuda
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_CUDA=OFF ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$CMAKE_THCOUNT"
