a289b5d19e58d1a375a334d12c05fe6048872a17
venkat0907 <venkatnallamothu0907@gmail.com>
2023-04-26 15:38:41 +0530

    intial commit

---
+++ base/include/FacialLandmarksCV.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Module.h"
+
+class Detail;
+class DetailSSD;
+class DetailHCASCADE;
+
+class FacialLandmarkCVProps : public ModuleProps
+{
+public:
+	enum FaceDetectionModelType
+	{
+		SSD,
+		HAAR_CASCADE
+	};
+
+	FacialLandmarkCVProps() : ModuleProps() {}
+
+	FacialLandmarkCVProps(FaceDetectionModelType _type) : ModuleProps()
+	{
+		type = _type;
+	}
+
+	FaceDetectionModelType type;
+};
+
+class FacialLandmarkCV : public Module
+{
+ public:
+	FacialLandmarkCV(FacialLandmarkCVProps props);
+	virtual ~FacialLandmarkCV();
+	bool init();
+	bool term();
+	void setProps(FacialLandmarkCVProps& props);
+	FacialLandmarkCVProps getProps();
+
+protected:
+	bool process(frame_container &frames);
+	bool processSOS(frame_sp &frame);
+	bool validateInputPins();
+	bool validateOutputPins();
+	void addInputPin(framemetadata_sp &metadata, string &pinId); // throws exception if validation fails
+	bool shouldTriggerSOS();
+	bool processEOS(string &pinId);
+	boost::shared_ptr<Detail> mDetail;
+	FacialLandmarkCVProps mProp;
+	std::string mOutputPinId1;
+	std::string mOutputPinId2;
+};
---
+++ base/test/faciallandmarkscv_tests.cpp
@@ -0,0 +1,135 @@
+#include <boost/test/unit_test.hpp>
+#include <opencv2/core/types.hpp>
+#include "FileReaderModule.h"
+#include "ExternalSinkModule.h"
+#include "FrameMetadata.h"
+#include "FrameMetadataFactory.h"
+#include "Frame.h"
+#include "Logger.h"
+#include "AIPExceptions.h"
+#include "FacialLandmarkCV.h"
+#include "test_utils.h"
+#include "Frame.h"
+#include "ApraPoint2f.h"
+
+BOOST_AUTO_TEST_SUITE(faciallandmarkcv_tests)
+
+class ExternalSinkProps : public ModuleProps
+{
+public:
+	ExternalSinkProps() : ModuleProps()
+	{
+	}
+
+};
+
+class ExternalSink : public Module
+{
+
+private:
+	std::string m_log;
+
+public:
+	ExternalSink(ExternalSinkProps props) : Module(SINK, "ExternalSink", props)
+	{
+	}
+
+	std::string getLog() const
+	{
+		return m_log;
+	}
+
+	frame_container pop()
+	{
+		return Module::pop();
+	}
+
+	boost::shared_ptr<FrameContainerQueue> getQue()
+	{
+		return Module::getQue();
+	}
+
+	~ExternalSink()
+	{
+	}
+
+protected:
+	bool process(frame_container& frames)
+	{
+		for (const auto& pair : frames)
+		{
+			LOG_INFO << pair.first << "," << pair.second;
+		}
+
+		auto frame = Module::getFrameByType(frames, FrameMetadata::FrameType::FACE_LANDMARKS_INFO);
+		if (frame)
+			LOG_INFO << "Timestamp <" << frame->timestamp << ">";
+
+		vector<vector<ApraPoint2f>> facelandmarks;
+		Utils::deSerialize<std::vector<std::vector<ApraPoint2f>>>(facelandmarks, frame->data(), frame->size());
+
+		for (int i = 0; i < facelandmarks.size(); i++)
+		{
+			m_log += "Facial landmarks for face #" + std::to_string(i) + ":\n";
+			for (int j = 0; j < facelandmarks[i].size(); j++)
+			{
+				ApraPoint2f p(facelandmarks[i][j]);
+				m_log += "Point #" + std::to_string(j) + ": (" + std::to_string(p.x) + "," + std::to_string(p.y) + ")\n";
+			}
+			assert(facelandmarks[i].size() == 68); // check if there are 68 facial landmarks
+		}
+
+		cv::Mat iImg = cv::imread("./data/faces.jpg");
+
+		for (int i = 0; i < facelandmarks.size(); i++)
+		{
+			for (int j = 0; j < facelandmarks[i].size(); j++)
+			{
+				ApraPoint2f p(facelandmarks[i][j]);
+				circle(iImg, p, 3, cv::Scalar(255, 200, 0), cv::LineTypes::FILLED);
+			}
+		}
+		std::vector<uchar> buf;
+		cv::imencode(".png", iImg, buf);
+
+	   Test_Utils::saveOrCompare("./data/testOutput/facesResult.png", buf.data(), buf.size(), 0);
+	
+	   return true;
+	}
+
+	bool validateInputPins()
+	{
+		return true;
+	}
+
+	bool validateInputOutputPins()
+	{
+		return true;
+	}
+
+};
+
+boost::shared_ptr<ExternalSink> sink;
+
+BOOST_AUTO_TEST_CASE(multiple_faces)
+{
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(FileReaderModuleProps("./data/faces.raw")));
+	auto metadata = framemetadata_sp(new RawImageMetadata(1024,768, ImageMetadata::ImageType::RGB, CV_8UC3, 0, CV_8U, FrameMetadata::HOST, true));
+	fileReader->addOutputPin(metadata);
+
+	auto facemark = boost::shared_ptr<FacialLandmarkCV>(new FacialLandmarkCV(FacialLandmarkCVProps(FacialLandmarkCVProps::FaceDetectionModelType::HAAR_CASCADE)));
+	fileReader->setNext(facemark);
+
+	auto sink = boost::shared_ptr<ExternalSink>(new ExternalSink(ExternalSinkProps()));
+	facemark->setNext(sink);
+
+	BOOST_TEST(fileReader->init());
+	BOOST_TEST(facemark->init());
+	BOOST_TEST(sink->init());
+
+	fileReader->step();
+	facemark->step();
+	sink->step();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
