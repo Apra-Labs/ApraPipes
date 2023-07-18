0e09bb48fbd565646a71e895bf912d6462f9431c
zaki <zakik@apra.in>
2023-07-18 14:51:31 +0530

    Taking size from getSerialize() now

--- base/src/MotionVectorExtractor.cpp
+++ base/src/MotionVectorExtractor.cpp
@@ -191,8 +191,8 @@ int DetailFfmpeg::decodeAndGetMotionVectors(AVPacket* pkt, frame_container& fram
 				{
 					DrawingOverlay drawingOverlay;
 					drawingOverlay.add(&compositeOverlay);
-
-					outFrame = makeFrameWithPinId(sideData->size, motionVectorPinId);
+					auto serializedSize = drawingOverlay.mGetSerializeSize();
+					outFrame = makeFrameWithPinId(serializedSize, motionVectorPinId);
 					memcpy(outFrame->data(), sideData->data, sideData->size);
 					drawingOverlay.serialize(outFrame);
 					frames.insert(make_pair(motionVectorPinId, outFrame));
@@ -284,7 +284,7 @@ void DetailOpenH264::getMotionVectors(frame_container& frames, frame_sp& outFram
 		{
 			DrawingOverlay drawingOverlay;
 			drawingOverlay.add(&compositeOverlay);
-			auto mvSize = static_cast<size_t>(mMotionVectorSize);
+			auto mvSize = drawingOverlay.mGetSerializeSize();
 			outFrame = makeframe(outFrame, mvSize, motionVectorPinId);
 			drawingOverlay.serialize(outFrame);
 			frames.insert(make_pair(motionVectorPinId, outFrame));
