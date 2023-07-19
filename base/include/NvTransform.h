7aa90fe6a7a00e5846fab0e317a0c445d91e96b7
yashrajsapra <88146397+yashrajsapra@users.noreply.github.com>
2023-07-19 04:44:02 -0700

    Updated NvTransform to do scale, crop and color transform  (#176)
    * Updated NvTransform to do scale, crop and transform ,
    Ability to pass filter as props
    
    * Passing correct value in metadata
    
    ---------
    
    Co-authored-by: Mradul Dubey <mradul@apra.in>

--- base/include/NvTransform.h
+++ base/include/NvTransform.h
@@ -5,20 +5,56 @@
 class NvTransformProps : public ModuleProps
 {
 public:
+	enum NvTransformFilter
+	{
+		NEAREST=0, // transform filter nearest.
+		BILINEAR,  // transform filter bilinear.
+		TAP_5,     // transform filter 5 tap.
+		TAP_10,    // transform filter 10 tap.
+		SMART,     // transform filter smart.
+		NICEST     // transform filter nicest.
+	};
+
 	NvTransformProps(ImageMetadata::ImageType _imageType) : top(0) , left(0) , width(0) , height(0)
 	{
 		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
 	}
 	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height) : top(0) , left(0) , width(_width) , height(_height)
 	{
 		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
+	}
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType) : top(0) , left(0) , width(_width) , height(_height)
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = _scaleHeight;
+		scaleWidth = _scaleWidth;
 	}
 	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height,  int _top , int _left) : top(_top) , left(_left) , width(_width) , height(_height)
 	{
 		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
+	}
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height,  int _top , int _left, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType) : top(_top) , left(_left) , width(_width) , height(_height)
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = _scaleHeight;
+		scaleWidth = _scaleWidth;
 	}
 	ImageMetadata::ImageType imageType;	
 	int top,left,width,height;
+	NvTransformFilter filterType;
+	float scaleWidth, scaleHeight; // scaleWidth and scaleHeight are factor of width and height , 
+								   //1 means no change 0.5 means half of actual dimension ,2 means twice of actual dimension	
 };
 
 class NvTransform : public Module
