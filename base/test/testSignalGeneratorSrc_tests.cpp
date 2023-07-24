fb65ee980fe6ece8534a219548df4c7407062c93
Vinayak Bhustali <vinayakb@apra.in>
2023-07-24 17:07:38 +0530

    Updating tolerance to 50

--- base/test/testSignalGeneratorSrc_tests.cpp
+++ base/test/testSignalGeneratorSrc_tests.cpp
@@ -46,7 +46,7 @@ BOOST_AUTO_TEST_CASE(Basic)
     BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE_PLANAR);
     const uint8_t* pReadDataTest = const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data()));
     unsigned int readDataSizeTest = outputFrame->size();
-    Test_Utils::saveOrCompare("./data/TestSample.raw", pReadDataTest, readDataSizeTest, 10);
+    Test_Utils::saveOrCompare("./data/TestSample.raw", pReadDataTest, readDataSizeTest, 50);
 }
 
 BOOST_AUTO_TEST_CASE(getSetProps)
@@ -69,7 +69,7 @@ BOOST_AUTO_TEST_CASE(getSetProps)
     BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE_PLANAR);
     const uint8_t* pReadDataTest = const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data()));
     unsigned int readDataSizeTest = outputFrame->size();
-    Test_Utils::saveOrCompare("./data/TestSample1.raw", pReadDataTest,readDataSizeTest, 10);
+    Test_Utils::saveOrCompare("./data/TestSample1.raw", pReadDataTest,readDataSizeTest, 50);
     TestSignalGeneratorProps newProps(400, 400);
     source->setProps(newProps);
     source->step();
