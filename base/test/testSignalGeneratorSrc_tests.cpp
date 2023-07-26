c619189a23d591ff2b301a694f7f058e0f38270d
Vinayak Bhustali <vinayakb@apra.in>
2023-07-26 11:25:31 +0530

    small fix in testcase

--- base/test/testSignalGeneratorSrc_tests.cpp
+++ base/test/testSignalGeneratorSrc_tests.cpp
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE(getSetProps)
     BOOST_TEST(outputFrame->getMetadata()->getFrameType() == FrameMetadata::RAW_IMAGE_PLANAR);
     const uint8_t* pReadDataTest = const_cast<const uint8_t *>(static_cast<uint8_t *>(outputFrame->data()));
     unsigned int readDataSizeTest = outputFrame->size();
-    Test_Utils::saveOrCompare("./data/TestSample1.raw", pReadDataTest,readDataSizeTest, 100);
+    Test_Utils::saveOrCompare("./data/TestSample1.raw", pReadDataTest,readDataSizeTest, 0);
     TestSignalGeneratorProps newProps(400, 400);
     source->setProps(newProps);
     source->step();
