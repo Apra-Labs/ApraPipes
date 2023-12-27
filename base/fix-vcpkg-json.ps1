acd8a1f91683b92a663b22c00f3035aa341a2a29
Kashyap Jois <kjois@iprdgroup.com>
2023-12-27 21:36:17 +0530

    Fix PS to remove whisper from vcpkg json

--- base/fix-vcpkg-json.ps1
+++ base/fix-vcpkg-json.ps1
@@ -1,7 +1,17 @@
 #inplace fixing of a vcpkg file
-param([String]$fileName='vcpkg.json', [switch]$removeOpenCV,  [switch]$removeCUDA, [switch]$onlyOpenCV)
+param([String]$fileName='vcpkg.json', [switch]$removeOpenCV,  [switch]$removeCUDA, [switch]$onlyOpenCV, [switch]$removeWhisper)
 
 $v = Get-Content $fileName -raw | ConvertFrom-Json
+
+if($removeWhisper.IsPresent){
+    $result = $v.overrides | Where-Object { $_.name -ne 'whisper' } 
+    # Check if the result is $null or empty and set it to an empty array if it is
+    if (-not $result) {
+        $result = @()
+    }
+    $v.overrides = $result
+    $v.dependencies = $v.dependencies | Where-Object { $_ -ne 'whisper'}
+}
 
 if ($removeCUDA.IsPresent)
 {
