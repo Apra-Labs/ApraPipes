6d4528e90a9634348829f42c08381a5250c20845
Kashyap Jois <kjois@iprdgroup.com>
2023-12-31 17:44:21 +0530

    Revert sound_record_tests.cpp changes

--- base/test/sound_record_tests.cpp
+++ base/test/sound_record_tests.cpp
@@ -18,18 +18,18 @@ BOOST_AUTO_TEST_SUITE(sound_record_tests)
 
 BOOST_AUTO_TEST_CASE(recordMono, *boost::unit_test::disabled())
 {
-    std::vector<std::string> audioFiles = { "./data/AudiotestMono.wav" };
-    Test_Utils::FileCleaner f(audioFiles);
+	std::vector<std::string> audioFiles = { "./data/AudiotestMono.wav" };
+	Test_Utils::FileCleaner f(audioFiles);
     // Manual test, listen to the file on audacity to for sanity check
     Logger::setLogLevel(boost::log::trivial::severity_level::info);
 
     auto time_to_run = Test_Utils::getArgValue("s", "10");
     auto n_seconds = atoi(time_to_run.c_str());
-    auto sampling_rate = 16000;
+    auto sampling_rate = 48000;
     auto channels = 1;
     auto sample_size_byte = 2;
 
-    AudioCaptureSrcProps sourceProps(sampling_rate, channels, 0, 200);
+    AudioCaptureSrcProps sourceProps(sampling_rate,channels,0,200);
     auto source = boost::shared_ptr<Module>(new AudioCaptureSrc(sourceProps));
 
     auto outputFile = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps(audioFiles[0], true)));
@@ -45,17 +45,17 @@ BOOST_AUTO_TEST_CASE(recordMono, *boost::unit_test::disabled())
     in_file_mono.seekg(0, ios::end);
     int file_size_mono = in_file_mono.tellg();
     BOOST_TEST((channels * sampling_rate * sample_size_byte * n_seconds >= file_size_mono - (file_size_mono * 0.02) &&
-        channels * sampling_rate * sample_size_byte * n_seconds <= file_size_mono + (file_size_mono * 0.02)));
+                channels * sampling_rate * sample_size_byte * n_seconds <= file_size_mono + (file_size_mono * 0.02)));
     p.stop();
     p.term();
     p.wait_for_all();
-    in_file_mono.close();
+	in_file_mono.close();
 }
 
 BOOST_AUTO_TEST_CASE(recordStereo, *boost::unit_test::disabled())
 {
-    std::vector<std::string> audioFiles = { "./data/AudiotestStereo.wav" };
-    Test_Utils::FileCleaner f(audioFiles);
+	std::vector<std::string> audioFiles = { "./data/AudiotestStereo.wav" };
+	Test_Utils::FileCleaner f(audioFiles);
     Logger::setLogLevel(boost::log::trivial::severity_level::info);
 
     auto time_to_run = Test_Utils::getArgValue("s", "10");
@@ -64,7 +64,7 @@ BOOST_AUTO_TEST_CASE(recordStereo, *boost::unit_test::disabled())
     auto channels = 2;
     auto sample_size_byte = 2;
 
-    AudioCaptureSrcProps sourceProps(sampling_rate, channels, 0, 200);
+    AudioCaptureSrcProps sourceProps(sampling_rate,channels,0,200);
     auto source = boost::shared_ptr<AudioCaptureSrc>(new AudioCaptureSrc(sourceProps));
 
     auto outputFile = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps(audioFiles[0], true)));
@@ -80,16 +80,16 @@ BOOST_AUTO_TEST_CASE(recordStereo, *boost::unit_test::disabled())
     in_file_stereo.seekg(0, ios::end);
     int file_size_stereo = in_file_stereo.tellg();
     BOOST_TEST((channels * sampling_rate * sample_size_byte * n_seconds >= file_size_stereo - (file_size_stereo * 0.02) &&
-        channels * sampling_rate * sample_size_byte * n_seconds <= file_size_stereo + (file_size_stereo * 0.02)));
+                channels * sampling_rate * sample_size_byte * n_seconds <= file_size_stereo + (file_size_stereo * 0.02)));
     p.stop();
     p.term();
     p.wait_for_all();
-    in_file_stereo.close();
+	in_file_stereo.close();
 }
 BOOST_AUTO_TEST_CASE(recordMonoStereo, *boost::unit_test::disabled())
 {
-    std::vector<std::string> audioFiles = { "./data/AudiotestMono.wav", "./data/AudiotestStereo.wav" };
-    Test_Utils::FileCleaner f(audioFiles);
+	std::vector<std::string> audioFiles = { "./data/AudiotestMono.wav", "./data/AudiotestStereo.wav" };
+	Test_Utils::FileCleaner f(audioFiles);
     Logger::setLogLevel(boost::log::trivial::severity_level::info);
 
     auto time_to_run = Test_Utils::getArgValue("s", "10");
@@ -97,14 +97,14 @@ BOOST_AUTO_TEST_CASE(recordMonoStereo, *boost::unit_test::disabled())
     auto sampling_rate = 44000;
     auto channels = 1;
     auto sample_size_byte = 2;
-    AudioCaptureSrcProps sourceProps(sampling_rate, channels, 0, 200);
+    AudioCaptureSrcProps sourceProps(sampling_rate,channels,0,200);
     auto source = boost::shared_ptr<AudioCaptureSrc>(new AudioCaptureSrc(sourceProps));
 
     auto outputFile = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps(audioFiles[0], true)));
     source->setNext(outputFile);
 
     auto outputFile2 = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps(audioFiles[1], true)));
-    source->setNext(outputFile2, false);
+    source->setNext(outputFile2,false);
 
 
     PipeLine p("test");
@@ -117,25 +117,25 @@ BOOST_AUTO_TEST_CASE(recordMonoStereo, *boost::unit_test::disabled())
     in_file_mono.seekg(0, ios::end);
     int file_size_mono = in_file_mono.tellg();
     BOOST_TEST((channels * sampling_rate * sample_size_byte * n_seconds >= file_size_mono - (file_size_mono * 0.02) &&
-        channels * sampling_rate * sample_size_byte * n_seconds <= file_size_mono + (file_size_mono * 0.02)));
+                channels * sampling_rate * sample_size_byte * n_seconds <= file_size_mono + (file_size_mono * 0.02)));
 
     auto currentProps = source->getProps();
     currentProps.channels = 2;
     currentProps.sampleRate = 48000;
     source->setProps(currentProps);
-    source->relay(outputFile, false);
-    source->relay(outputFile2, true);
+    source->relay(outputFile,false);
+    source->relay(outputFile2,true);
     boost::this_thread::sleep_for(boost::chrono::seconds(n_seconds));
-    in_file_mono.close();
+	in_file_mono.close();
 
     ifstream in_file_stereo(audioFiles[1], ios::binary);
     in_file_stereo.seekg(0, ios::end);
     int file_size_stereo = in_file_stereo.tellg();
     BOOST_TEST((channels * sampling_rate * sample_size_byte * n_seconds >= file_size_stereo - (file_size_stereo * 0.02) &&
-        channels * sampling_rate * sample_size_byte * n_seconds <= file_size_stereo + (file_size_stereo * 0.02)));
+                channels * sampling_rate * sample_size_byte * n_seconds <= file_size_stereo + (file_size_stereo * 0.02)));
     p.stop();
     p.term();
     p.wait_for_all();
-    in_file_stereo.close();
+	in_file_stereo.close();
 }
 BOOST_AUTO_TEST_SUITE_END()
