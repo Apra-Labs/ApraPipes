91fe148899a20c21730db77b5366660779ff2106
Kashyap Jois <kjois@iprdgroup.com>
2023-12-31 19:11:41 +0530

    Revert changes to vcpkg indentation and remove Whisper option

--- base/fix-vcpkg-json.ps1
+++ base/fix-vcpkg-json.ps1
@@ -1,17 +1,7 @@
 #inplace fixing of a vcpkg file
-param([String]$fileName='vcpkg.json', [switch]$removeOpenCV,  [switch]$removeCUDA, [switch]$onlyOpenCV, [switch]$removeWhisper)
+param([String]$fileName='vcpkg.json', [switch]$removeOpenCV,  [switch]$removeCUDA, [switch]$onlyOpenCV)
 
 $v = Get-Content $fileName -raw | ConvertFrom-Json
-
-if($removeWhisper.IsPresent){
-    $result = $v.overrides | Where-Object { $_.name -ne 'whisper' } 
-    # Check if the result is $null or empty and set it to an empty array if it is
-    if (-not $result) {
-        $result = @()
-    }
-    $v.overrides = $result
-    $v.dependencies = $v.dependencies | Where-Object { $_ -ne 'whisper'}
-}
 
 if ($removeCUDA.IsPresent)
 {
