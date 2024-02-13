874febb58cc09536ca60863220ac736994854a80
Kushal Jain <kushalj@apra.in>
2024-02-13 18:20:11 +0530

    added features to nv_transform module

--- base/include/NvTransform.h
+++ base/include/NvTransform.h
@@ -2,6 +2,32 @@
 
 #include "Module.h"
 
+class CropRect : public ModuleProps
+{
+public:
+	CropRect(int _top, int _left, int _width, int _height) : top(_top), left(_left), width(_width), height(_height) {}
+
+	int top;
+	int left;
+	int width;
+	int height;
+
+	size_t getSerializeSize()
+	{
+		return ModuleProps::getSerializeSize() + sizeof(int) * 4;
+	}
+
+private:
+	friend class boost::serialization::access;
+
+	template <class Archive>
+	void serialize(Archive &ar, const unsigned int version)
+	{
+		ar &boost::serialization::base_object<ModuleProps>(*this);
+		ar &top &left &width &height ;
+	}	
+};
+
 class NvTransformProps : public ModuleProps
 {
 public:
@@ -15,46 +41,171 @@ public:
 		NICEST     // transform filter nicest.
 	};
 
-	NvTransformProps(ImageMetadata::ImageType _imageType) : top(0) , left(0) , width(0) , height(0)
+	enum NvTransformRotate
+	{
+		ROTATE_0 = 0, // rotate 0 degress
+		ANTICLOCKWISE_ROTATE_90, // rotate 90 degrees anticlockwise
+		ANTICLOCKWISE_ROTATE_180, // rotate 180 degrees anticlockwise
+		ANTICLOCKWISE_ROTATE_270, // rotate 270 degrees anticlockwise
+		CLOCKWISE_ROTATE_90, // rotate 90 degrees clockwise
+		CLOCKWISE_ROTATE_180, // rotate 180 degrees clockwise
+		CLOCKWISE_ROTATE_270, // rotate 270 degrees clockwise
+	};
+
+	NvTransformProps(ImageMetadata::ImageType _imageType) : output_width(0), output_height(0), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
 	{
 		imageType = _imageType;
 		scaleHeight = 1;
 		scaleWidth = 1;
 		filterType = NvTransformFilter::SMART;
+		rotateType = NvTransformRotate::ROTATE_0;
 	}
-	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height) : top(0) , left(0) , width(_width) , height(_height)
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, NvTransformRotate _rotateType) : output_width(0), output_height(0), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
 	{
 		imageType = _imageType;
 		scaleHeight = 1;
 		scaleWidth = 1;
 		filterType = NvTransformFilter::SMART;
+		rotateType = _rotateType;
 	}
-	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType) : top(0) , left(0) , width(_width) , height(_height)
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height) : output_width(_width) , output_height(_height), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
+		rotateType = NvTransformRotate::ROTATE_0;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, NvTransformRotate _rotateType) : output_width(_width), output_height(_height), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
+		rotateType = _rotateType;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, NvTransformFilter _filterType, NvTransformRotate _rotateType) : output_width(_width) , output_height(_height), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = _filterType;
+		rotateType = _rotateType;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType) : output_width(_width) , output_height(_height), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
 	{
 		imageType = _imageType;
 		filterType = _filterType;
 		scaleHeight = _scaleHeight;
 		scaleWidth = _scaleWidth;
+		rotateType = NvTransformRotate::ROTATE_0;
 	}
-	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height,  int _top , int _left) : top(_top) , left(_left) , width(_width) , height(_height)
-	{
-		imageType = _imageType;
-		scaleHeight = 1;
-		scaleWidth = 1;
-		filterType = NvTransformFilter::SMART;
-	}
-	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height,  int _top , int _left, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType) : top(_top) , left(_left) , width(_width) , height(_height)
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType, NvTransformRotate _rotateType) : output_width(_width) , output_height(_height), src_rect({0,0,0,0}), dst_rect({0,0,0,0})
 	{
 		imageType = _imageType;
 		filterType = _filterType;
 		scaleHeight = _scaleHeight;
 		scaleWidth = _scaleWidth;
+		rotateType = _rotateType;
 	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, CropRect _src_rect) : output_width(_width) , output_height(_height), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
+		rotateType = NvTransformRotate::ROTATE_0;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, CropRect _src_rect, NvTransformRotate _rotateType) : output_width(_width) , output_height(_height), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		filterType = NvTransformFilter::SMART;
+		rotateType = _rotateType;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, CropRect _src_rect, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType) : output_width(_width) , output_height(_height), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = _scaleHeight;
+		scaleWidth = _scaleWidth;
+		rotateType = NvTransformRotate::ROTATE_0;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, CropRect _src_rect, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType, NvTransformRotate _rotateType) : output_width(0) , output_height(0), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = _scaleHeight;
+		scaleWidth = _scaleWidth;
+		rotateType = _rotateType;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, CropRect _src_rect, NvTransformFilter _filterType, NvTransformRotate _rotateType) : output_width(0) , output_height(0), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		rotateType = _rotateType;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, CropRect _src_rect, float _scaleWidth, float _scaleHeight, NvTransformFilter _filterType, NvTransformRotate _rotateType) : output_width(_width) , output_height(_height), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = _scaleHeight;
+		scaleWidth = _scaleWidth;
+		rotateType = _rotateType;
+	}
+
+	NvTransformProps(ImageMetadata::ImageType _imageType, int _width, int _height, CropRect _src_rect, NvTransformFilter _filterType, NvTransformRotate _rotateType) : output_width(_width) , output_height(_height), src_rect(_src_rect), dst_rect({0,0,0,0})
+	{
+		imageType = _imageType;
+		filterType = _filterType;
+		scaleHeight = 1;
+		scaleWidth = 1;
+		rotateType = _rotateType;
+	}
+
 	ImageMetadata::ImageType imageType;	
-	int top,left,width,height;
+	int output_width, output_height;
 	NvTransformFilter filterType;
 	float scaleWidth, scaleHeight; // scaleWidth and scaleHeight are factor of width and height , 
-								   //1 means no change 0.5 means half of actual dimension ,2 means twice of actual dimension	
+								   //1 means no change 0.5 means half of actual dimension ,2 means twice of actual dimension
+
+	NvTransformRotate rotateType;
+	CropRect src_rect, dst_rect;
+
+	size_t getSerializeSize()
+	{
+		return ModuleProps::getSerializeSize() + sizeof(int) * 2 + sizeof(float) * 2 + sizeof(src_rect) * 2 + sizeof(imageType) + sizeof(filterType) + sizeof(rotateType);
+	}
+
+private:
+	friend class boost::serialization::access;
+
+	template <class Archive>
+	void serialize(Archive &ar, const unsigned int version)
+	{
+		ar &boost::serialization::base_object<ModuleProps>(*this);
+		ar &output_width &output_height ;
+		ar &scaleWidth &scaleHeight ;
+		ar &src_rect &dst_rect ;
+		ar& imageType;
+		ar& filterType;
+		ar& rotateType;
+	}	
 };
 
 class NvTransform : public Module
@@ -66,6 +217,9 @@ public:
 	bool init();
 	bool term();
 
+	void setProps(NvTransformProps& props);
+	NvTransformProps getProps();
+
 protected:
 	bool process(frame_container& frames);
 	bool processSOS(frame_sp& frame);
@@ -73,6 +227,8 @@ protected:
 	bool validateOutputPins();
 	void addInputPin(framemetadata_sp& metadata, string& pinId); // throws exception if validation fails
 	bool processEOS(string& pinId);
+
+	bool handlePropsChange(frame_sp& frame);
 
 private:
 	void setMetadata(framemetadata_sp& metadata);
--- base/test/nvtransform_tests.cpp
+++ base/test/nvtransform_tests.cpp
@@ -20,6 +20,9 @@
 #include "DMAUtils.h"
 #include "NvArgusCamera.h"
 
+#include "FileWriterModule.h"
+#include "DMAFDToHostCopy.h"
+
 #include <chrono>
 
 using sys_clock = std::chrono::system_clock;
@@ -166,6 +169,216 @@ BOOST_AUTO_TEST_CASE(cropAndScale, *boost::unit_test::disabled())
 
 	p.run_all_threaded();
 	boost::this_thread::sleep_for(boost::chrono::seconds(10));
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(scaleTest, *boost::unit_test::disabled())
+{
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	NvV4L2CameraProps sourceProps(1280, 720, 10);
+	auto source = boost::shared_ptr<Module>(new NvV4L2Camera(sourceProps));
+
+	float scaleHeight = 2;
+	float scaleWidth = 2;
+	auto nv_transform = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 1280, 720, scaleWidth, scaleHeight, NvTransformProps::NvTransformFilter::NICEST, NvTransformProps::NvTransformRotate::ROTATE_0)));
+	source->setNext(nv_transform);
+
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0,0,1)));
+	nv_transform->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(source);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(cropTest, *boost::unit_test::disabled())
+{
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	NvV4L2CameraProps sourceProps(1280, 720, 10);
+	auto source = boost::shared_ptr<Module>(new NvV4L2Camera(sourceProps));
+
+	auto nv_transform = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 720, 720, {0, 280, 720, 720}, NvTransformProps::NvTransformFilter::SMART, NvTransformProps::NvTransformRotate::ROTATE_0)));
+	source->setNext(nv_transform);
+
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0,0,1)));
+	nv_transform->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(source);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(scaleAndCropTest, *boost::unit_test::disabled())
+{
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	NvV4L2CameraProps sourceProps(1280, 720, 10);
+	auto source = boost::shared_ptr<Module>(new NvV4L2Camera(sourceProps));
+
+	float scaleHeight = 2;
+	float scaleWidth = 2;
+	auto nv_transform = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 720, 720, {0, 280, 720, 720}, scaleWidth, scaleHeight, NvTransformProps::NvTransformFilter::NICEST, NvTransformProps::NvTransformRotate::ROTATE_0)));
+	source->setNext(nv_transform);
+
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0,0,1)));
+	nv_transform->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(source);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(scaleAndCropAndRotate180Test, *boost::unit_test::disabled())
+{
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	NvV4L2CameraProps sourceProps(1280, 720, 10);
+	auto source = boost::shared_ptr<Module>(new NvV4L2Camera(sourceProps));
+
+	float scaleHeight = 2;
+	float scaleWidth = 2;
+	auto nv_transform = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 720, 720, {0, 280, 720, 720}, scaleWidth, scaleHeight, NvTransformProps::NvTransformFilter::NICEST, NvTransformProps::NvTransformRotate::CLOCKWISE_ROTATE_180)));
+	source->setNext(nv_transform);
+
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0,0,1)));
+	nv_transform->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(source);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(getSetPropsTest, *boost::unit_test::disabled())
+{
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	NvV4L2CameraProps sourceProps(1280, 720, 10);
+	auto source = boost::shared_ptr<Module>(new NvV4L2Camera(sourceProps));
+
+	float scaleHeight = 2;
+	float scaleWidth = 2;
+	auto nv_transform = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 720, 720, {0, 280, 720, 720}, scaleWidth, scaleHeight, NvTransformProps::NvTransformFilter::BILINEAR, NvTransformProps::NvTransformRotate::ROTATE_0)));
+	source->setNext(nv_transform);
+
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0,0,1)));
+	nv_transform->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(source);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+
+	auto propsChange = nv_transform->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::CLOCKWISE_ROTATE_180;
+	nv_transform->setProps(propsChange);
+
+	boost::this_thread::sleep_for(boost::chrono::seconds(5));
+	p.stop();
+	p.term();
+	p.wait_for_all();
+}
+
+BOOST_AUTO_TEST_CASE(allRotateTest, *boost::unit_test::disabled())
+{
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	// 1. Source 1280x720
+	NvV4L2CameraProps sourceProps(1280, 720, 10);
+	auto source = boost::shared_ptr<Module>(new NvV4L2Camera(sourceProps));
+
+	// 2. Take Input from Source (1280 x 720) and apply Nv Transform to center crop source to 720 x 720
+	auto nv_transform_1 = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 720, 720, {0, 280, 720, 720}, NvTransformProps::NvTransformFilter::SMART, NvTransformProps::NvTransformRotate::ROTATE_0)));
+	source->setNext(nv_transform_1);
+
+	// 3. Take Input Nv Transform 1 (720x720)and apply Nv Transform to scale to 1000 x 1000
+	auto nv_transform_2 = boost::shared_ptr<NvTransform>(new NvTransform(NvTransformProps(ImageMetadata::RGBA, 1000, 1000, NvTransformProps::NvTransformFilter::NICEST, NvTransformProps::NvTransformRotate::ROTATE_0)));
+	nv_transform_1->setNext(nv_transform_2);
+
+	// 4. EGL Renderer to display real time stream
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0,0,1)));
+	nv_transform_2->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(source);
+	BOOST_TEST(p.init());
+
+	p.run_all_threaded();
+	auto propsChange = nv_transform_2->getProps();
+	
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::ROTATE_0;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::CLOCKWISE_ROTATE_90;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::CLOCKWISE_ROTATE_180;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::CLOCKWISE_ROTATE_270;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::ROTATE_0;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::ANTICLOCKWISE_ROTATE_90;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::ANTICLOCKWISE_ROTATE_180;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+
+	propsChange = nv_transform_2->getProps();
+	propsChange.rotateType = NvTransformProps::NvTransformRotate::ANTICLOCKWISE_ROTATE_270;
+	nv_transform_2->setProps(propsChange);
+	boost::this_thread::sleep_for(boost::chrono::seconds(2));
+	
 	p.stop();
 	p.term();
 	p.wait_for_all();
