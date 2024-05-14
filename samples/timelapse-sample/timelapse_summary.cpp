213bf2c326efc51e34cfe79ac2427172080f5645
AdityaKBhadragond14 <adityab@iprdgroup.com>
2024-05-14 10:25:33 +0530

    added main to sample cpp file.

--- samples/timelapse-sample/timelapse_summary.cpp
+++ samples/timelapse-sample/timelapse_summary.cpp
@@ -16,7 +16,7 @@
 TimelapsePipeline::TimelapsePipeline() : pipeline("test") {}
 
 bool TimelapsePipeline::setupPipeline() {
-    std::string outFolderPath = "./data/timeplase_videos/";
+  std::string outFolderPath = "C:\\Workspace\\ApraPipesFork\\ApraPipes\\data\\timeplase_videos";
     auto cuContext = apracucontext_sp(new ApraCUcontext());
     uint32_t gopLength = 25;
     uint32_t bitRateKbps = 1000;
@@ -25,7 +25,7 @@ bool TimelapsePipeline::setupPipeline() {
     bool enableBFrames = false;
     bool sendDecodedFrames = true;
     bool sendOverlayFrames = false;
-    std::string videoPath = "./data/Mp4_videos/h264_video_metadata/20230514/0011/video1.mp4";
+    std::string videoPath = "C:\\Workspace\\ApraPipesFork\\ApraPipes\\data\\Mp4_videos\\h264_video_metadata\\20230514\\0011\\video1.mp4";
     auto h264ImageMetadata = framemetadata_sp(new H264Metadata(0, 0));
     auto mp4ReaderProps = Mp4ReaderSourceProps(videoPath, false, 0, true, false, false);
     mp4ReaderProps.fps = 24;
@@ -35,7 +35,7 @@ bool TimelapsePipeline::setupPipeline() {
     mp4Reader->addOutPutPin(mp4Metadata);
     std::vector<std::string> mImagePin;
     mImagePin = mp4Reader->getAllOutputPinsByType(FrameMetadata::H264_DATA);
-    auto motionExtractor = boost::shared_ptr<MotionVectorExtractor>(new MotionVectorExtractor(MotionVectorExtractorProps(MotionVectorExtractorProps::MVExtractMethod::OPENH264,sendDecodedFrames, 50, sendOverlayFrames)));
+    auto motionExtractor = boost::shared_ptr<MotionVectorExtractor>(new MotionVectorExtractor(MotionVectorExtractorProps(MotionVectorExtractorProps::MVExtractMethod::OPENH264,sendDecodedFrames, 20, sendOverlayFrames)));
     mp4Reader -> setNext(motionExtractor, mImagePin);
 
     auto colorchange1 = boost::shared_ptr<ColorConversion>(new ColorConversion(ColorConversionProps(ColorConversionProps::BGR_TO_RGB)));
@@ -72,4 +72,31 @@ bool TimelapsePipeline::stopPipeline() {
     pipeline.term();
     pipeline.wait_for_all();
     return true;
+}
+
+int main() {
+  TimelapsePipeline pipelineInstance;
+
+  // Setup the pipeline
+  if (!pipelineInstance.setupPipeline()) {
+    std::cerr << "Failed to setup pipeline." << std::endl;
+    return 1; // Or any error code indicating failure
+  }
+
+  // Start the pipeline
+  if (!pipelineInstance.startPipeline()) {
+    std::cerr << "Failed to start pipeline." << std::endl;
+    return 1; // Or any error code indicating failure
+  }
+
+  // Wait for the pipeline to run for 10 seconds
+  boost::this_thread::sleep_for(boost::chrono::seconds(10));
+
+  // Stop the pipeline
+  if (!pipelineInstance.stopPipeline()) {
+    std::cerr << "Failed to stop pipeline." << std::endl;
+    return 1; // Or any error code indicating failure
+  }
+
+  return 0; // Or any success code
 }
