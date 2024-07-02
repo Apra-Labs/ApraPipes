00def41bb4794a169e4ef7a2b233a1973dc9c1cb
Ankush Jain <ankushj@apra.in>
2024-07-02 19:02:43 +0530

    changes related to validating pins and comment of stdafx

--- base/include/stdafx.h
+++ base/include/stdafx.h
@@ -2,13 +2,21 @@
 // // or project specific include files that are used frequently, but
 // // are changed infrequently
 // //
+// // stdafx.h : include file for standard system include files,
+// // or project specific include files that are used frequently, but
+// // are changed infrequently
+// //
 
+// #pragma once
 // #pragma once
 
 // #ifndef LINUX
 // #include "targetver.h"
+// #ifndef LINUX
+// #include "targetver.h"
 
-// #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from
-// Windows headers #endif
+// #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// #endif
+
 
 // // TODO: reference additional headers your program requires here
