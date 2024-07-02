19c3fb6c79bc0cb965934140a2ec46539d98887e
Vinayak Y B <vinayakb@apra.in>
2024-07-02 19:14:09 +0530

    Added sleep changes in paused state while loop

--- base/include/RTSPPusher.h
+++ base/include/RTSPPusher.h
@@ -41,6 +41,7 @@ public:
 	boost::shared_ptr<Frame> savedIFrame;
 	boost::thread pauserThread;
 	void pauserThreadFunction();
+	void setFps(int fps);
 protected:
 	bool process(frame_container &frames);
 	bool validateInputPins();
