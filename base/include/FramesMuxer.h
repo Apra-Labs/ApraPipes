cbaaca5d527f2ba5c7dc7e0670532cc9a9c1dc48
yashrajsapra <yashrajs@apra.in>
2024-07-22 19:38:16 +0530

    Added TimeStamp Strategy in Frames Muxer

--- base/include/FramesMuxer.h
+++ base/include/FramesMuxer.h
@@ -7,18 +7,22 @@ class FramesMuxerProps : public ModuleProps
 public:
 	enum Strategy {
 		ALL_OR_NONE,
-		MAX_DELAY_ANY
+		MAX_DELAY_ANY,
+		MAX_TIMESTAMP_DELAY
 	};
+
 public:
-	FramesMuxerProps() : ModuleProps() 
+	FramesMuxerProps() : ModuleProps()
 	{
+		maxTsDelayInMS = 16.67;
 		maxDelay = 30;
-		strategy = ALL_OR_NONE;
+		strategy = MAX_TIMESTAMP_DELAY;
 		fIndexStrategyType = FIndexStrategy::FIndexStrategyType::NONE;
 	}
 
 	int maxDelay; // Difference between current frame and first frame in the queue
 	Strategy strategy;
+	double maxTsDelayInMS; // Max TimeStampDelay in Milli Seconds
 };
 
 class FramesMuxerStrategy;
