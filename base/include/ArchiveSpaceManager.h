be867268838ad888d4e777b4a8f4ba31a8e5b4f9
Vinayak Y B <vinayakb@apra.in>
2024-07-31 14:03:05 +0530

    commented some lines in asm

--- base/include/ArchiveSpaceManager.h
+++ base/include/ArchiveSpaceManager.h
@@ -12,61 +12,62 @@ public:
     samplingFreq = _samplingFreq;
     fps = 0.001;
 
-    auto totalSpace = boost::filesystem::space(pathToWatch);
-    if ((lowerWaterMark > upperWaterMark) ||
-        (upperWaterMark > totalSpace.capacity)) {
-      LOG_ERROR << "Please enter correct properties!";
-      std::string errorMsg =
-          "Incorrect properties set for Archive Manager. TotalDiskCapacity <" +
-          std::to_string(totalSpace.capacity) + ">lowerWaterMark<" +
-          std::to_string(lowerWaterMark) + "> UpperWaterMark<" +
-          std::to_string(upperWaterMark) + ">";
-      throw AIPException(AIP_FATAL, errorMsg);
-    }
+    // auto totalSpace = std::space(pathToWatch);
+    // if ((lowerWaterMark > upperWaterMark) ||
+    //     (upperWaterMark > totalSpace.capacity)) {
+    //   LOG_ERROR << "Please enter correct properties!";
+    //   std::string errorMsg =
+    //       "Incorrect properties set for Archive Manager. TotalDiskCapacity <"
+    //       + std::to_string(totalSpace.capacity) + ">lowerWaterMark<" +
+    //       std::to_string(lowerWaterMark) + "> UpperWaterMark<" +
+    //       std::to_string(upperWaterMark) + ">";
+    //   throw AIPException(AIP_FATAL, errorMsg);
   }
+}
 
-  ArchiveSpaceManagerProps(uint64_t maxSizeAllowed, string _pathToWatch,
-                           int _samplingFreq) {
-    lowerWaterMark = maxSizeAllowed - (maxSizeAllowed / 10);
-    upperWaterMark = maxSizeAllowed;
-    pathToWatch = _pathToWatch;
-    samplingFreq = _samplingFreq;
-    fps = 0.001;
+ArchiveSpaceManagerProps(uint64_t maxSizeAllowed, string _pathToWatch,
+                         int _samplingFreq) {
+  lowerWaterMark = maxSizeAllowed - (maxSizeAllowed / 10);
+  upperWaterMark = maxSizeAllowed;
+  pathToWatch = _pathToWatch;
+  samplingFreq = _samplingFreq;
+  fps = 0.001;
 
-    auto totalSpace = boost::filesystem::space(pathToWatch);
-    if ((lowerWaterMark > upperWaterMark) ||
-        (upperWaterMark > totalSpace.capacity)) {
-      LOG_ERROR << "Please enter correct properties!";
-      std::string errorMsg =
-          "Incorrect properties set for Archive Manager. TotalDiskCapacity <" +
-          std::to_string(totalSpace.capacity) + ">lowerWaterMark<" +
-          std::to_string(lowerWaterMark) + "> UpperWaterMark<" +
-          std::to_string(upperWaterMark) + ">";
-      throw AIPException(AIP_FATAL, errorMsg);
-    }
+  auto totalSpace = boost::filesystem::space(pathToWatch);
+  if ((lowerWaterMark > upperWaterMark) ||
+      (upperWaterMark > totalSpace.capacity)) {
+    LOG_ERROR << "Please enter correct properties!";
+    std::string errorMsg =
+        "Incorrect properties set for Archive Manager. TotalDiskCapacity <" +
+        std::to_string(totalSpace.capacity) + ">lowerWaterMark<" +
+        std::to_string(lowerWaterMark) + "> UpperWaterMark<" +
+        std::to_string(upperWaterMark) + ">";
+    throw AIPException(AIP_FATAL, errorMsg);
   }
+}
 
-  uint64_t lowerWaterMark; // Lower disk space
-  uint64_t upperWaterMark; // Higher disk space
-  std::string pathToWatch;
-  int samplingFreq;
-  size_t getSerializeSize() {
-    return ModuleProps::getSerializeSize() + sizeof(lowerWaterMark) +
-           sizeof(upperWaterMark) + sizeof(pathToWatch) + sizeof(samplingFreq);
-  }
+uint64_t lowerWaterMark; // Lower disk space
+uint64_t upperWaterMark; // Higher disk space
+std::string pathToWatch;
+int samplingFreq;
+size_t getSerializeSize() {
+  return ModuleProps::getSerializeSize() + sizeof(lowerWaterMark) +
+         sizeof(upperWaterMark) + sizeof(pathToWatch) + sizeof(samplingFreq);
+}
 
 private:
-  friend class boost::serialization::access;
+friend class boost::serialization::access;
 
-  template <class Archive>
-  void serialize(Archive &ar, const unsigned int version) {
-    ar &boost::serialization::base_object<ModuleProps>(*this);
-    ar & lowerWaterMark;
-    ar & upperWaterMark;
-    ar & pathToWatch;
-    ar & samplingFreq;
-  }
-};
+template <class Archive>
+void serialize(Archive &ar, const unsigned int version) {
+  ar &boost::serialization::base_object<ModuleProps>(*this);
+  ar & lowerWaterMark;
+  ar & upperWaterMark;
+  ar & pathToWatch;
+  ar & samplingFreq;
+}
+}
+;
 
 class ArchiveSpaceManager : public Module {
 public:
