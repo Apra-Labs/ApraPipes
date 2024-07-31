78ba6b304e56e3c21c6a8c14a72159395ff691e9
Vinayak Y B <vinayakb@apra.in>
2024-07-31 13:28:12 +0530

    disp space manager updated

--- base/include/ArchiveSpaceManager.h
+++ base/include/ArchiveSpaceManager.h
@@ -1,90 +1,94 @@
 #pragma once
-#include "Module.h"
 #include "AIPExceptions.h"
+#include "Module.h"
 
-class ArchiveSpaceManagerProps : public ModuleProps
-{
+class ArchiveSpaceManagerProps : public ModuleProps {
 public:
-	ArchiveSpaceManagerProps(uint32_t _lowerWaterMark, uint32_t _upperWaterMark, string _pathToWatch, int _samplingFreq)
-	{
-		lowerWaterMark = _lowerWaterMark;
-		upperWaterMark = _upperWaterMark;
-		pathToWatch = _pathToWatch; 
-		samplingFreq = _samplingFreq;
-		fps = 0.001;
-  
-		auto totalSpace = boost::filesystem::space(pathToWatch);
-		if ((lowerWaterMark > upperWaterMark) || (upperWaterMark > totalSpace.capacity))
-		{
-			LOG_ERROR << "Please enter correct properties!";
-			std::string errorMsg = "Incorrect properties set for Archive Manager. TotalDiskCapacity <" + std::to_string(totalSpace.capacity) + ">lowerWaterMark<" + std::to_string(lowerWaterMark) + "> UpperWaterMark<" + std::to_string(upperWaterMark) + ">";
-			throw AIPException(AIP_FATAL, errorMsg);
-		}
-	}
+  ArchiveSpaceManagerProps(uint64_t _lowerWaterMark, uint64_t _upperWaterMark,
+                           string _pathToWatch, int _samplingFreq) {
+    lowerWaterMark = _lowerWaterMark;
+    upperWaterMark = _upperWaterMark;
+    pathToWatch = _pathToWatch;
+    samplingFreq = _samplingFreq;
+    fps = 0.001;
 
-	ArchiveSpaceManagerProps(uint32_t maxSizeAllowed, string _pathToWatch, int _samplingFreq)
-	{
-		lowerWaterMark = maxSizeAllowed - (maxSizeAllowed / 10);
-		upperWaterMark = maxSizeAllowed;
-		pathToWatch = _pathToWatch;
-		samplingFreq = _samplingFreq;
-		fps = 0.001;
+    auto totalSpace = boost::filesystem::space(pathToWatch);
+    if ((lowerWaterMark > upperWaterMark) ||
+        (upperWaterMark > totalSpace.capacity)) {
+      LOG_ERROR << "Please enter correct properties!";
+      std::string errorMsg =
+          "Incorrect properties set for Archive Manager. TotalDiskCapacity <" +
+          std::to_string(totalSpace.capacity) + ">lowerWaterMark<" +
+          std::to_string(lowerWaterMark) + "> UpperWaterMark<" +
+          std::to_string(upperWaterMark) + ">";
+      throw AIPException(AIP_FATAL, errorMsg);
+    }
+  }
 
-		auto totalSpace = boost::filesystem::space(pathToWatch);
-		if ((lowerWaterMark > upperWaterMark) || (upperWaterMark > totalSpace.capacity))
-		{
-			LOG_ERROR << "Please enter correct properties!";
-			std::string errorMsg = "Incorrect properties set for Archive Manager. TotalDiskCapacity <" + std::to_string(totalSpace.capacity) + ">lowerWaterMark<" + std::to_string(lowerWaterMark) + "> UpperWaterMark<" + std::to_string(upperWaterMark) + ">";
-			throw AIPException(AIP_FATAL, errorMsg);
-		}
-	}
+  ArchiveSpaceManagerProps(uint64_t maxSizeAllowed, string _pathToWatch,
+                           int _samplingFreq) {
+    lowerWaterMark = maxSizeAllowed - (maxSizeAllowed / 10);
+    upperWaterMark = maxSizeAllowed;
+    pathToWatch = _pathToWatch;
+    samplingFreq = _samplingFreq;
+    fps = 0.001;
 
+    auto totalSpace = boost::filesystem::space(pathToWatch);
+    if ((lowerWaterMark > upperWaterMark) ||
+        (upperWaterMark > totalSpace.capacity)) {
+      LOG_ERROR << "Please enter correct properties!";
+      std::string errorMsg =
+          "Incorrect properties set for Archive Manager. TotalDiskCapacity <" +
+          std::to_string(totalSpace.capacity) + ">lowerWaterMark<" +
+          std::to_string(lowerWaterMark) + "> UpperWaterMark<" +
+          std::to_string(upperWaterMark) + ">";
+      throw AIPException(AIP_FATAL, errorMsg);
+    }
+  }
 
-	uint32_t lowerWaterMark; // Lower disk space
-	uint32_t upperWaterMark; // Higher disk space
-	std::string pathToWatch;
-	int samplingFreq;
-	size_t getSerializeSize()
-	{
-		return ModuleProps::getSerializeSize() + sizeof(lowerWaterMark) + sizeof(upperWaterMark) + sizeof(pathToWatch) + sizeof(samplingFreq);
-	}
+  uint64_t lowerWaterMark; // Lower disk space
+  uint64_t upperWaterMark; // Higher disk space
+  std::string pathToWatch;
+  int samplingFreq;
+  size_t getSerializeSize() {
+    return ModuleProps::getSerializeSize() + sizeof(lowerWaterMark) +
+           sizeof(upperWaterMark) + sizeof(pathToWatch) + sizeof(samplingFreq);
+  }
+
 private:
-	friend class boost::serialization::access;
+  friend class boost::serialization::access;
 
-	template <class Archive>
-	void serialize(Archive& ar, const unsigned int version)
-	{
-		ar& boost::serialization::base_object<ModuleProps>(*this);
-		ar& lowerWaterMark;
-		ar& upperWaterMark;
-		ar& pathToWatch;
-		ar& samplingFreq;
-	}
+  template <class Archive>
+  void serialize(Archive &ar, const unsigned int version) {
+    ar &boost::serialization::base_object<ModuleProps>(*this);
+    ar & lowerWaterMark;
+    ar & upperWaterMark;
+    ar & pathToWatch;
+    ar & samplingFreq;
+  }
 };
-
 
 class ArchiveSpaceManager : public Module {
 public:
-	ArchiveSpaceManager(ArchiveSpaceManagerProps _props);
+  ArchiveSpaceManager(ArchiveSpaceManagerProps _props);
 
-	virtual ~ArchiveSpaceManager() {
-	}
-	bool init();
-	bool term();
-	uint32_t finalArchiveSpace = 0;
-	void setProps(ArchiveSpaceManagerProps& props);
-	ArchiveSpaceManagerProps getProps();
+  virtual ~ArchiveSpaceManager() {}
+  bool init();
+  bool term();
+  uint64_t finalArchiveSpace = 0;
+  void setProps(ArchiveSpaceManagerProps &props);
+  ArchiveSpaceManagerProps getProps();
 
 protected:
-	bool process();
-	bool validateInputPins();
-	bool validateOutputPins();
-	bool validateInputOutputPins();
-	void addInputPin(framemetadata_sp& metadata, string& pinId);
-	bool handlePropsChange(frame_sp& frame);
-private:
+  bool produce();
+  bool validateInputPins();
+  bool validateOutputPins();
+  bool validateInputOutputPins();
+  void addInputPin(framemetadata_sp &metadata, string &pinId);
+  bool handlePropsChange(frame_sp &frame);
 
-	class Detail;
-	boost::shared_ptr<Detail> mDetail;
-	bool checkDirectory = true;
+private:
+  class Detail;
+  boost::shared_ptr<Detail> mDetail;
+  bool checkDirectory = true;
 };
--- base/src/ArchiveSpaceManager.cpp
+++ base/src/ArchiveSpaceManager.cpp
@@ -1,188 +1,162 @@
-#include <stdafx.h>
-#include<cstdlib>
-#include <boost/filesystem.hpp>
-#include "Module.h"
 #include "ArchiveSpaceManager.h"
+#include "Module.h"
+#include <boost/filesystem.hpp>
+#include <cstdlib>
+#include <stdafx.h>
 
-class ArchiveSpaceManager::Detail
-{
+class ArchiveSpaceManager::Detail {
 public:
-    Detail(ArchiveSpaceManagerProps& _props) : mProps(_props)
-    {
-    }
+  Detail(ArchiveSpaceManagerProps &_props) : mProps(_props) {}
 
-    ~Detail()
-    {
-    }
+  ~Detail() {}
 
-    void setProps(ArchiveSpaceManagerProps _props)
-    {
-        mProps = _props;
-    }
+  void setProps(ArchiveSpaceManagerProps _props) { mProps = _props; }
 
-    uint32_t estimateDirectorySize(boost::filesystem::path _dir)
-    {
-        uint32_t dirSize = 0;
-        int sample = 0;
-        int inCount = 0;
-        int countFreq = 0;
-        uint32_t tempSize = 0;
+  uint64_t estimateDirectorySize(boost::filesystem::path _dir) {
+    std::cout << "Estimating size for directory: " << _dir << std::endl;
+    uint64_t dirSize = 0;
+    int sample = 0;
+    int inCount = 0;
+    int countFreq = 0;
+    uint64_t tempSize = 0;
 
-        for (const auto& entry : boost::filesystem::recursive_directory_iterator(_dir))
-        {
-            if ((boost::filesystem::is_regular_file(entry)))
-            {
-                if (!(countFreq % mProps.samplingFreq))
-                {
-                    sample = (rand() % mProps.samplingFreq) + 1;
-                    inCount = 0;
-                }
-                if (inCount == sample)
-                {
-                    tempSize = boost::filesystem::file_size(entry);
-                    dirSize += tempSize * mProps.samplingFreq;
-                }
-                inCount++;
-                countFreq++;
-            }
+    for (const auto &entry :
+         boost::filesystem::recursive_directory_iterator(_dir)) {
+      if (boost::filesystem::is_regular_file(entry)) {
+        if (countFreq % mProps.samplingFreq == 0) {
+          sample = (rand() % mProps.samplingFreq);
+          inCount = 0;
         }
-        if (inCount < mProps.samplingFreq)
-        {
-            dirSize = dirSize + (tempSize * inCount);
+
+        if (inCount == sample) {
+          try {
+            tempSize = boost::filesystem::file_size(entry);
+            dirSize += tempSize * mProps.samplingFreq;
+          } catch (const std::exception &e) {
+            std::cout << "Failed to get file size for " << entry << ": "
+                      << e.what() << std::endl;
+          }
         }
-        return dirSize;
+
+        inCount++;
+        countFreq++;
+      }
     }
 
-    boost::filesystem::path getOldestDirectory(boost::filesystem::path _cam)
-    {
-        for (const auto& camFolder : boost::filesystem::directory_iterator(_cam))
-        {
-            for (const auto& folder : boost::filesystem::recursive_directory_iterator(camFolder))
-            {
-                if (boost::filesystem::is_regular_file(folder))
-                {
-                    boost::filesystem::path p = folder.path().parent_path();
-                    return p;
-                }
-            }
+    // Adjust for the remaining files if they are fewer than the sampling
+    // frequency
+    if (inCount < mProps.samplingFreq && inCount > 0) {
+      dirSize += tempSize * inCount;
+    }
+
+    std::cout << "Total Directory Size: " << dirSize << std::endl;
+    return dirSize;
+  }
+
+  boost::filesystem::path getOldestDirectory(boost::filesystem::path _cam) {
+    for (const auto &camFolder : boost::filesystem::directory_iterator(_cam)) {
+      for (const auto &folder :
+           boost::filesystem::recursive_directory_iterator(camFolder)) {
+        if (boost::filesystem::is_regular_file(folder)) {
+          boost::filesystem::path p = folder.path().parent_path();
+          return p;
         }
-        return _cam;
+      }
+    }
+    return _cam;
+  };
+
+  void manageDirectory() {
+    auto comparator = [](std::pair<boost::filesystem::path, uint64_t> &a,
+                         std::pair<boost::filesystem::path, uint64_t> &b) {
+      return a.second < b.second;
     };
+    while (archiveSize > mProps.lowerWaterMark) {
+      for (const auto &camFolder :
+           boost::filesystem::directory_iterator(mProps.pathToWatch)) {
+        boost::filesystem::path oldHrDir = getOldestDirectory(camFolder);
+        foldVector.push_back(
+            {oldHrDir, boost::filesystem::last_write_time(oldHrDir)});
+      }
+      sort(foldVector.begin(), foldVector.end(),
+           comparator); // Sorting the vector
 
-    void manageDirectory()
-    {
-        auto comparator = [](std::pair<boost::filesystem::path, uint32_t>& a, std::pair<boost::filesystem::path, uint32_t>& b) {return a.second < b.second; };
-        while (archiveSize > mProps.lowerWaterMark)
-        {
-            for (const auto& camFolder : boost::filesystem::directory_iterator(mProps.pathToWatch))
-            {
-                boost::filesystem::path oldHrDir = getOldestDirectory(camFolder);
-                foldVector.push_back({ oldHrDir,boost::filesystem::last_write_time(oldHrDir) });
-            }
-            sort(foldVector.begin(), foldVector.end(), comparator); //Sorting the vector
-
-            uint32_t tempSize = 0;
-            boost::filesystem::path delDir = foldVector[0].first;
-            BOOST_LOG_TRIVIAL(info) << "Deleting file : " << delDir.string();
-            tempSize = estimateDirectorySize(delDir);
-            archiveSize = archiveSize - tempSize;
-            try
-            {
-                boost::filesystem::remove_all(delDir);
-            }
-            catch (...)
-            {
-                LOG_ERROR << "Could not delete directory!..";
-            }
-            foldVector.clear();
-        }
+      uint64_t tempSize = 0;
+      boost::filesystem::path delDir = foldVector[0].first;
+      BOOST_LOG_TRIVIAL(info) << "Deleting file : " << delDir.string();
+      tempSize = estimateDirectorySize(delDir);
+      archiveSize = archiveSize - tempSize;
+      try {
+        boost::filesystem::remove_all(delDir);
+      } catch (...) {
+        LOG_ERROR << "Could not delete directory!..";
+      }
+      foldVector.clear();
     }
+  }
 
-    uint32_t diskOperation()
-    {
-        archiveSize = estimateDirectorySize(mProps.pathToWatch);
-        if (archiveSize > mProps.upperWaterMark)
-        {
-            manageDirectory();
-        }
-        uint32_t tempSize = archiveSize;
-        archiveSize = 0;
-        return tempSize;
+  uint64_t diskOperation() {
+    archiveSize = estimateDirectorySize(mProps.pathToWatch);
+    if (archiveSize > mProps.upperWaterMark) {
+      manageDirectory();
+    } else {
+      LOG_INFO << "DiskSpace is under range";
     }
-    ArchiveSpaceManagerProps mProps;
-    uint32_t archiveSize = 0;
-    std::vector<std::pair<boost::filesystem::path, uint32_t>> foldVector;
+    uint64_t tempSize = archiveSize;
+    archiveSize = 0;
+    return tempSize;
+  }
+  ArchiveSpaceManagerProps mProps;
+  uint64_t archiveSize = 0;
+  std::vector<std::pair<boost::filesystem::path, uint64_t>> foldVector;
 };
 
-
 ArchiveSpaceManager::ArchiveSpaceManager(ArchiveSpaceManagerProps _props)
-    :Module(SOURCE, "ArchiveSpaceManager", _props)
-{
-    mDetail.reset(new Detail(_props));
+    : Module(SOURCE, "ArchiveSpaceManager", _props) {
+  mDetail.reset(new Detail(_props));
 }
 
-bool ArchiveSpaceManager::validateInputPins()
-{
-    return true;
+bool ArchiveSpaceManager::validateInputPins() { return true; }
+
+bool ArchiveSpaceManager::validateOutputPins() { return true; }
+
+bool ArchiveSpaceManager::validateInputOutputPins() { return true; }
+
+void ArchiveSpaceManager::addInputPin(framemetadata_sp &metadata,
+                                      string &pinId) {
+  Module::addInputPin(metadata, pinId);
+  Module::addOutputPin(metadata, pinId);
 }
 
-bool ArchiveSpaceManager::validateOutputPins()
-{
-    return true;
+bool ArchiveSpaceManager::init() {
+  if (!Module::init()) {
+    return false;
+  }
+  return true;
 }
 
-bool ArchiveSpaceManager::validateInputOutputPins()
-{
-    return true;
+bool ArchiveSpaceManager::term() { return Module::term(); }
+
+ArchiveSpaceManagerProps ArchiveSpaceManager::getProps() {
+  return mDetail->mProps;
 }
 
-void ArchiveSpaceManager::addInputPin(framemetadata_sp& metadata, string& pinId)
-{
-    Module::addInputPin(metadata, pinId);
-    Module::addOutputPin(metadata, pinId);
+void ArchiveSpaceManager::setProps(ArchiveSpaceManagerProps &props) {
+  Module::addPropsToQueue(props);
 }
 
-bool ArchiveSpaceManager::init()
-{
-    if (!Module::init())
-    {
-        return false;
-    }
-    return true;
+bool ArchiveSpaceManager::handlePropsChange(frame_sp &frame) {
+  ArchiveSpaceManagerProps props(mDetail->mProps);
+  auto ret = Module::handlePropsChange(frame, props);
+  mDetail->setProps(props);
+  return ret;
 }
 
-bool ArchiveSpaceManager::term()
-{
-    return Module::term();
-}
-
-ArchiveSpaceManagerProps ArchiveSpaceManager::getProps()
-{
-    return mDetail->mProps;
-}
-
-void ArchiveSpaceManager::setProps(ArchiveSpaceManagerProps& props)
-{
-    Module::addPropsToQueue(props);
-}
-
-bool ArchiveSpaceManager::handlePropsChange(frame_sp& frame)
-{
-    ArchiveSpaceManagerProps props(mDetail->mProps);
-    auto ret = Module::handlePropsChange(frame, props);
-    mDetail->setProps(props);
-    return ret;
-}
-
-bool ArchiveSpaceManager::process()
-{
-    try
-    {
-        finalArchiveSpace = mDetail->diskOperation();
-    }
-    catch (...)
-    {
-        LOG_ERROR << "Archive Disk Manager encountered an error.";
-    }
-    return true;
+bool ArchiveSpaceManager::produce() {
+  try {
+    finalArchiveSpace = mDetail->diskOperation();
+  } catch (...) {
+    LOG_ERROR << "Archive Disk Manager encountered an error.";
+  }
+  return true;
 }
