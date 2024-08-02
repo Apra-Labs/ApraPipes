9c2f0a3236f4aa9793d76225d693ae0092b5a8ee
zaki <zakik@apra.in>
2024-08-02 18:35:32 +0530

    Updated the libmp4 port for getting avcc data

--- thirdparty/custom-overlay/libmp4/portfile.cmake
+++ thirdparty/custom-overlay/libmp4/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Apra-Labs/libmp4
-    REF caaf26d4ed4f7d731fb0e65fbba2ea9b250d75d1
-    SHA512 f7ad8384517b816bbd80a3c06b394996b4be431f7d50bd8420c8ad820af84b2c112b3c6dec7d0b74e6303875d4c26bc48e77dbcbf4bfc78ddf4bda5e25f0af91
+    REF 98f8ae9637093c822f344ec95c8cffbb814dd336
+    SHA512 34c8ced415b5b1e03c0b04148ca5647109a70226af1fdc3c0739c8d88e68294ebe187a59d44008d5bea3fbab7e09b19e311a03712710f62b53444a92e924db4c
     HEAD_REF forApraPipes
 )
 vcpkg_configure_cmake(
