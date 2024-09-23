cdbdc9970da455bfa049a4771f873d00390c3af4
yashrajsapra <yashrajs@apra.in>
2024-09-23 16:56:49 +0530

    -> Added GetProps & SetProps API's in RTSP Client Source Module -> Added Test where Updating RTSP Url works

--- base/src/RTSPClientSrc.cpp
+++ base/src/RTSPClientSrc.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-
 #include <iostream>
 #include <string>
 
@@ -24,7 +23,6 @@ extern "C"
 #include <libavdevice/avdevice.h>
 }
 
-
 FrameMetadata::FrameType getFrameType_fromFFMPEG(AVMediaType avMediaType, AVCodecID avCodecID)
 {
     if (avMediaType == AVMEDIA_TYPE_VIDEO)
@@ -40,36 +38,57 @@ FrameMetadata::FrameType getFrameType_fromFFMPEG(AVMediaType avMediaType, AVCode
         case AV_CODEC_ID_BMP:
             return FrameMetadata::BMP_IMAGE;
         default:
-            return  FrameMetadata::GENERAL;
+            return FrameMetadata::GENERAL;
         }
     }
-    return  FrameMetadata::GENERAL; //for everything else we may not have a match
+    return FrameMetadata::GENERAL; // for everything else we may not have a match
 }
 
 class RTSPClientSrc::Detail
 {
 public:
-    Detail(RTSPClientSrc* m,std::string path, bool useTCP, int urlTimeout) :myModule(m), path(path), bConnected(false), bUseTCP(useTCP), iUrlTimeout(urlTimeout)
+    Detail(RTSPClientSrc *m, std::string path, bool useTCP, int urlTimeout) : myModule(m), path(path), bConnected(false), bUseTCP(useTCP), iUrlTimeout(urlTimeout)
     {
-		int_ctx = std::make_pair(iUrlTimeout, start_time);
-		int_cb = { interrupt_cb, &int_ctx };
-	}
-    ~Detail() { destroy(); }
+        int_ctx = std::make_pair(iUrlTimeout, start_time);
+        int_cb = {interrupt_cb, &int_ctx};
+    }
+
+    ~Detail() 
+    { 
+        destroy();
+    }
+
     void destroy()
     {
-        if(nullptr!= pFormatCtx)
+        if (nullptr != pFormatCtx)
+        {
             avformat_close_input(&pFormatCtx);
+        }
     }
 
     // https://stackoverflow.com/questions/10666242/detecting-a-timeout-in-ffmpeg
     static int interrupt_cb(void *ctx)
     {
-        std::pair<int, time_t>* context = static_cast<std::pair<int, time_t>*>(ctx);
+        std::pair<int, time_t> *context = static_cast<std::pair<int, time_t> *>(ctx);
         int timeout = context->first;
         time_t start_time = context->second;
         if ((time(NULL) - start_time) > timeout)
+        {
             return 1; // Return 1 to interrupt the operation
-        return 0; 
+        }
+        return 0;
+    }
+
+    bool updateDetailStateOnPropsChange(RTSPClientSrcProps props)
+    {
+        LOG_DEBUG << "Updated Props==>>";
+        LOG_DEBUG << "Updated RTSP Url " << props.rtspURL << " == Updated Connection Type TCP " << props.useTCP << " == URL TimeOut ==" << props.timeout << " ==";
+        path = props.rtspURL;
+        bUseTCP = props.useTCP;
+        iUrlTimeout = props.timeout;
+        int_ctx = std::make_pair(iUrlTimeout, start_time);
+        int_cb = {interrupt_cb, &int_ctx};
+        return true;
     }
 
     bool connect()
@@ -77,25 +96,25 @@ public:
         avformat_network_init();
         av_register_all();
 
-        //Start time initialize
-		int_ctx.second = time(NULL);
+        // Start time initialize
+        int_ctx.second = time(NULL);
 
         pFormatCtx = avformat_alloc_context();
-		pFormatCtx->interrupt_callback = int_cb;
+        pFormatCtx->interrupt_callback = int_cb;
 
-        AVDictionary* avdic = NULL;
+        AVDictionary *avdic = NULL;
 
-        av_dict_set(&avdic, "rtsp_transport", (bUseTCP)?"tcp":"udp", 0);
+        av_dict_set(&avdic, "rtsp_transport", (bUseTCP) ? "tcp" : "udp", 0);
         av_dict_set(&avdic, "max_delay", "100", 0);
 
         if (avformat_open_input(&pFormatCtx, path.c_str(), NULL, &avdic) != 0)
         {
-            LOG_ERROR << "can't open the URL." << path <<std::endl;
+            LOG_ERROR << "can't open the URL." << path << std::endl;
             bConnected = false;
             return bConnected;
         }
 
-		int_ctx.second = time(NULL);
+        int_ctx.second = time(NULL);
 
         if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
         {
@@ -104,27 +123,27 @@ public:
             return bConnected;
         }
         LOG_INFO << "Opened url and found stream info";
-        //this loop should check that each output pin is satisfied
+        // this loop should check that each output pin is satisfied
         for (unsigned int i = 0; i < pFormatCtx->nb_streams; i++)
         {
             auto pCodecCtx = pFormatCtx->streams[i]->codec;
-            LOG_INFO << av_get_media_type_string (pCodecCtx->codec_type)<<" Codec: " << avcodec_get_name(pCodecCtx->codec_id);
-            auto fType=getFrameType_fromFFMPEG(pCodecCtx->codec_type,pCodecCtx->codec_id);
-            string outPin=myModule->getOutputPinIdByType(fType);
+            LOG_INFO << av_get_media_type_string(pCodecCtx->codec_type) << " Codec: " << avcodec_get_name(pCodecCtx->codec_id);
+            auto fType = getFrameType_fromFFMPEG(pCodecCtx->codec_type, pCodecCtx->codec_id);
+            string outPin = myModule->getOutputPinIdByType(fType);
             if (!outPin.empty())
             {
                 streamsMap[i] = outPin;
                 if (pCodecCtx->codec_type == AVMEDIA_TYPE_VIDEO)
                 {
                     videoStream = i;
-                    auto meta= FrameMetadataFactory::downcast<H264Metadata>(myModule->getOutputMetadata(outPin));
+                    auto meta = FrameMetadataFactory::downcast<H264Metadata>(myModule->getOutputMetadata(outPin));
                     H264Metadata tmp(pCodecCtx->width, pCodecCtx->height, pCodecCtx->gop_size, pCodecCtx->max_b_frames);
                     meta->setData(tmp);
                 }
             }
         }
 
-        //by this time all the output pins should be satisfied if not we have a problem
+        // by this time all the output pins should be satisfied if not we have a problem
         if (streamsMap.size() < myModule->getNumberOfOutputPins())
         {
             LOG_ERROR << "some output pins can not be satsified" << std::endl;
@@ -137,7 +156,7 @@ public:
 
     void saveSpsPps()
     {
-        H264Utils::extractSpsAndPpsFromExtradata((char*)pFormatCtx->streams[0]->codec->extradata, pFormatCtx->streams[0]->codec->extradata_size, spsData, spsSize, ppsData, ppsSize);
+        H264Utils::extractSpsAndPpsFromExtradata((char *)pFormatCtx->streams[0]->codec->extradata, pFormatCtx->streams[0]->codec->extradata_size, spsData, spsSize, ppsData, ppsSize);
     }
 
     frame_sp prependSpsPpsToFrame(std::string id, size_t offsetToSkip, int naluSeparatorSize)
@@ -147,7 +166,7 @@ public:
         size_t totalFrameSize = packet.size + spsSize + ppsSize;
 
         auto frm = myModule->makeFrame(totalFrameSize, id);
-        uint8_t* frameData = static_cast<uint8_t*>(frm->data());
+        uint8_t *frameData = static_cast<uint8_t *>(frm->data());
         memcpy(frameData, spsData, spsSize);
         frameData += spsSize;
         memcpy(frameData, ppsData, ppsSize);
@@ -158,40 +177,41 @@ public:
 
     bool readBuffer()
     {
-        if(!initDone)
+        if (!initDone)
         {
             std::chrono::time_point<std::chrono::system_clock> t = std::chrono::system_clock::now();
             beginTs = std::chrono::duration_cast<std::chrono::milliseconds>(t.time_since_epoch());
             initDone = true;
         }
-        
+
         frame_container outFrames;
         bool got_something = false;
         int count = 0;
-        while(!got_something)
+        while (!got_something)
         {
 
-			int_ctx.second = time(NULL);
+            int_ctx.second = time(NULL);
 
             if (av_read_frame(pFormatCtx, &packet) >= 0)
             {
-                if (videoStream >= 0) //source has video
+                if (videoStream >= 0) // source has video
                 {
-                    if (packet.stream_index == videoStream) //got video
+                    if (packet.stream_index == videoStream) // got video
                     {
                         got_something = true;
                     }
                 }
                 else
                 {
-                    got_something = true; //does not need video and got something
+                    got_something = true; // does not need video and got something
                 }
-              
+
                 size_t offset = 0;
                 int naluSeparatorSize = 0;
-                H264Utils::getNALUnitOffsetAndSizeBasedOnGivenType((char*)packet.data, packet.size, offset, naluSeparatorSize, H264Utils::H264_NAL_TYPE_IDR_SLICE);
+                H264Utils::getNALUnitOffsetAndSizeBasedOnGivenType((char *)packet.data, packet.size, offset, naluSeparatorSize, H264Utils::H264_NAL_TYPE_IDR_SLICE);
                 auto it = streamsMap.find(packet.stream_index);
-                if (it != streamsMap.end()) { // so we have an interest in sending this
+                if (it != streamsMap.end())
+                { // so we have an interest in sending this
                     frame_sp frm;
                     if (!offset && !naluSeparatorSize)
                     {
@@ -205,14 +225,14 @@ public:
                     }
 
                     std::chrono::time_point<std::chrono::system_clock> t = std::chrono::system_clock::now();
-                	auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t.time_since_epoch());
+                    auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t.time_since_epoch());
                     frm->timestamp = dur.count();
                     if (!outFrames.insert(make_pair(it->second, frm)).second)
                     {
                         LOG_WARNING << "oops! there is already another packet for pin " << it->second;
                     }
                     auto diff = dur - beginTs;
-                    if(diff.count() > 1000)
+                    if (diff.count() > 1000)
                     {
                         currentCameraFps = frameCount;
                         frameCount = 0;
@@ -223,21 +243,21 @@ public:
                 av_packet_unref(&packet);
             }
             else
-            {  //Inform control module about stopping of stream
-                LOG_INFO<<"Not getting data from source will retry - "<<count;
+            { // Inform control module about stopping of stream
+                LOG_INFO << "Not getting data from source will retry - " << count;
                 count++;
-                if(count == 10)
+                if (count == 10)
                 {
                     return false;
                 }
             }
         }
 
-        if(outFrames.size()>0)
-		{
-			LOG_TRACE << "sending out data from RTSPCLIENTSRC !";
-			myModule->send(outFrames);
-		}
+        if (outFrames.size() > 0)
+        {
+            LOG_TRACE << "sending out data from RTSPCLIENTSRC !";
+            myModule->send(outFrames);
+        }
         return true;
     }
 
@@ -245,24 +265,24 @@ public:
     int frameCount = 0;
     int currentCameraFps = 0;
     boost::shared_ptr<Module> controlModule = nullptr;
+
 private:
     AVPacket packet;
-    AVFormatContext* pFormatCtx = nullptr;
+    AVFormatContext *pFormatCtx = nullptr;
     std::string path;
     bool bConnected;
-    int videoStream=-1;
+    int videoStream = -1;
     bool bUseTCP;
-	int iUrlTimeout;
-	time_t start_time;
+    int iUrlTimeout;
+    time_t start_time;
     std::map<unsigned int, std::string> streamsMap;
-    RTSPClientSrc* myModule;
-	std::chrono::milliseconds beginTs;
-	bool initDone = false;
-    char* spsData = nullptr;
-    char* ppsData = nullptr;
+    RTSPClientSrc *myModule;
+    std::chrono::milliseconds beginTs;
+    bool initDone = false;
+    char *spsData = nullptr;
+    char *ppsData = nullptr;
     int spsSize = 0;
     int ppsSize = 0;
-    
     std::pair<int, time_t> int_ctx;
     AVIOInterruptCB int_cb;
 };
@@ -271,11 +291,13 @@ RTSPClientSrc::RTSPClientSrc(RTSPClientSrcProps _props) : Module(SOURCE, "RTSPCl
 {
     mDetail.reset(new Detail(this, mProps.rtspURL, mProps.useTCP, mProps.timeout));
 }
-RTSPClientSrc::~RTSPClientSrc() {
-	LOG_INFO << "Destructor called for rtspclientsrc";
+RTSPClientSrc::~RTSPClientSrc()
+{
+    LOG_TRACE << "Destructor called for rtspclientsrc";
 }
-bool RTSPClientSrc::init() {
-    if(mDetail->connect())
+bool RTSPClientSrc::init()
+{
+    if (mDetail->connect())
     {
         return Module::init();
     }
@@ -283,38 +305,33 @@ bool RTSPClientSrc::init() {
     executeErrorCallback(error);
     return false;
 }
-bool RTSPClientSrc::term() {
+bool RTSPClientSrc::term()
+{
     LOG_INFO << "Term called for rtspclientsrc";
-	mDetail.reset();
+    mDetail.reset();
     return true;
 }
-void RTSPClientSrc::setProps(RTSPClientSrcProps& props)
-{
-    mProps = props;
-    //TBD need to also reset the whole connection
-}
-RTSPClientSrcProps RTSPClientSrc::getProps() {
-    return mProps;
-}
 
-bool RTSPClientSrc::produce() { 
-	LOG_TRACE << "Produce called: starting readBuffer for RTSPCLIENTSRC";
+bool RTSPClientSrc::produce()
+{
+    LOG_TRACE << "Produce called: starting readBuffer for RTSPCLIENTSRC";
     auto ret = mDetail->readBuffer();
-	LOG_TRACE << "Produce called: finished readBuffer for RTSPCLIENTSRC";
-	return ret;
+    LOG_TRACE << "Produce called: finished readBuffer for RTSPCLIENTSRC";
+    return ret;
 }
-bool RTSPClientSrc::validateOutputPins() { 
-    //smallest check at least one output pin should be there
+bool RTSPClientSrc::validateOutputPins()
+{
+    // smallest check at least one output pin should be there
     return this->getNumberOfOutputPins() > 0;
 }
 void RTSPClientSrc::notifyPlay(bool play) {}
-bool RTSPClientSrc::handleCommand(Command::CommandType type, frame_sp& frame) 
+bool RTSPClientSrc::handleCommand(Command::CommandType type, frame_sp &frame)
 {
     if (type == Command::CommandType::Relay)
-	{
+    {
         return Module::handleCommand(type, frame);
     }
-    return true; 
+    return true;
 }
 
 int RTSPClientSrc::getCurrentFps()
@@ -322,4 +339,23 @@ int RTSPClientSrc::getCurrentFps()
     return mDetail->currentCameraFps;
 }
 
-bool RTSPClientSrc::handlePropsChange(frame_sp& frame) { return true; }
+bool RTSPClientSrc::handlePropsChange(frame_sp &frame)
+{
+    RTSPClientSrcProps props;
+    bool ret = Module::handlePropsChange(frame, props);
+    mProps = props;
+    mDetail->updateDetailStateOnPropsChange(mProps);
+    mDetail->connect();
+    return true;
+}
+
+void RTSPClientSrc::setProps(RTSPClientSrcProps &props)
+{
+    Module::addPropsToQueue(props);
+}
+
+RTSPClientSrcProps RTSPClientSrc::getProps()
+{
+    fillProps(mProps);
+    return mProps;
+}
--- base/test/rtsp_client_tests.cpp
+++ base/test/rtsp_client_tests.cpp
@@ -7,6 +7,9 @@
 #include "PipeLine.h"
 #include "H264Metadata.h"
 #include "FileWriterModule.h"
+#include "Mp4WriterSink.h"
+#include "PipeLine.h"
+
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE(rtsp_client_tests)
@@ -51,6 +54,82 @@ BOOST_AUTO_TEST_CASE(basic, *boost::unit_test::disabled())
 	m->term();
 	fw->term();
 
+}
+
+BOOST_AUTO_TEST_CASE(recording24x7, *boost::unit_test::disabled())
+{
+	rtsp_client_tests_data d;
+	std::string mp4Writer_247_Path = "./TestVideos/";
+	uint32_t mp4Writer_247_chunk = 1; // Default
+	uint32_t mp4Writer_247_sync = 2;
+	uint32_t mp4Writer_247_fps = 24;
+
+	// drop bunny/mp4 into evostream folder,
+	// also set it up for RTSP client authentication as shown here: https://sites.google.com/apra.in/development/home/evostream/rtsp-authentication?authuser=1
+	auto url = string("rtsp://10.102.10.79/axis-media/media.amp?resolution=1280x720");
+
+	auto source = boost::shared_ptr<RTSPClientSrc>(new RTSPClientSrc(RTSPClientSrcProps(url, d.empty, d.empty)));
+	auto meta = framemetadata_sp(new H264Metadata());
+	source->addOutputPin(meta);
+
+	// Mp4-Writer 24/7
+	Mp4WriterSinkProps mp4Writer_247_Props = Mp4WriterSinkProps(mp4Writer_247_chunk, mp4Writer_247_sync, mp4Writer_247_fps, mp4Writer_247_Path);
+	// mp4Writer_247_Props.logHealth = true;
+	// mp4Writer_247_Props.logHealthFrequency = 100;
+	boost::shared_ptr<Mp4WriterSink> mp4writer_247 = boost::shared_ptr<Mp4WriterSink>(new Mp4WriterSink(mp4Writer_247_Props));
+	source->setNext(mp4writer_247);
+
+	auto p = boost::shared_ptr<PipeLine>(new PipeLine("NVR_Core_Pipeline"));
+	p->appendModule(source);
+	bool pipelineInit = p->init();
+	if (!pipelineInit)
+	{
+		LOG_ERROR << "Core pipeline init failed. RTSP URL most likely the problem.";
+	}
+	p->run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(UINT32_MAX));
+	p->stop();
+	p->term();
+	p->wait_for_all();
+}
+
+
+BOOST_AUTO_TEST_CASE(getSetProps, *boost::unit_test::disabled())
+{
+	rtsp_client_tests_data d;
+	std::string mp4Writer_247_Path = "./TestVideos/";
+	uint32_t mp4Writer_247_chunk = 1; // Default
+	uint32_t mp4Writer_247_sync = 2;
+	uint32_t mp4Writer_247_fps = 24;
+
+	auto url = string("rtsp://10.102.10.79/axis-media/media.amp?resolution=1280x720");
+
+	auto source = boost::shared_ptr<RTSPClientSrc>(new RTSPClientSrc(RTSPClientSrcProps(url, d.empty, d.empty)));
+	auto meta = framemetadata_sp(new H264Metadata());
+	source->addOutputPin(meta);
+
+	Mp4WriterSinkProps mp4Writer_247_Props = Mp4WriterSinkProps(mp4Writer_247_chunk, mp4Writer_247_sync, mp4Writer_247_fps, mp4Writer_247_Path);
+	boost::shared_ptr<Mp4WriterSink> mp4writer_247 = boost::shared_ptr<Mp4WriterSink>(new Mp4WriterSink(mp4Writer_247_Props));
+	source->setNext(mp4writer_247);
+
+	auto p = boost::shared_ptr<PipeLine>(new PipeLine("NVR_Core_Pipeline"));
+	p->appendModule(source);
+	bool pipelineInit = p->init();
+	if (!pipelineInit)
+	{
+		LOG_ERROR << "Core pipeline init failed. RTSP URL most likely the problem.";
+	}
+	p->run_all_threaded();
+	boost::this_thread::sleep_for(boost::chrono::seconds(60));
+	RTSPClientSrcProps currProps = source->getProps();
+
+	auto updatedUrl = string("rtsp://root:m4m1g0@10.102.10.56/axis-media/media.amp?resolution=1280x720");
+	currProps.rtspURL = updatedUrl;
+	source->setProps(currProps);
+	boost::this_thread::sleep_for(boost::chrono::seconds(UINT32_MAX));
+	p->stop();
+	p->term();
+	p->wait_for_all();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
