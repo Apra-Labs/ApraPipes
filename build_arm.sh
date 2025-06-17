cd390282de8e693bf04efdf29a0c0ed0a0f4454d
yashrajsapra <yashrajs@apra.in>
2025-06-17 20:51:03 +0530

    Updated Build arm script to disable  arm flag

--- build_arm.sh
+++ build_arm.sh
@@ -23,5 +23,5 @@ cd ..
 CMAKE_THCOUNT=$(sh ./checkProc.sh)
 mkdir -p _build
 cd _build
-export VCPKG_FORCE_SYSTEM_BINARIES=1 && export VCPKG_OVERLAY_PORTS=../thirdparty/custom-overlay && cmake -B . -DENABLE_ARM_WITHOUT_JETSON=ON -DENABLE_CUDA=OFF -DENABLE_WINDOWS=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+export VCPKG_FORCE_SYSTEM_BINARIES=1 && export VCPKG_OVERLAY_PORTS=../thirdparty/custom-overlay && cmake -B . -DENABLE_ARM_WITHOUT_JETSON=ON -DENABLE_ARM64=OFF -DENABLE_CUDA=OFF -DENABLE_WINDOWS=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo ../base -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . -- -j "$(($(nproc) - 1))"
