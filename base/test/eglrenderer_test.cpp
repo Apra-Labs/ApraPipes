b0afae9f9db9a69826b72e4df8c1ee4e54344926
chidanand S M <chidanandm@apra.in>
2025-10-31 15:05:50 +0530

    Implement rotation and flip in NvTransform with new unit tests
    - Added rotation and flip functionality to NvTransform.
    - Created comprehensive Boost unit tests to validate all rotation (0°, 90°, 180°, 270°)
      and flip (horizontal, vertical, both) combinations.

--- base/test/eglrenderer_test.cpp
+++ base/test/eglrenderer_test.cpp
@@ -3,38 +3,51 @@
 #include "FileReaderModule.h"
 #include "EglRenderer.h"
 #include "PipeLine.h"
+#include "NvV4L2Camera.h"
+#include "NvTransform.h"
+
 
 BOOST_AUTO_TEST_SUITE(eglrenderer_tests)
 
-BOOST_AUTO_TEST_CASE(basic)
+BOOST_AUTO_TEST_CASE(basic, *boost::unit_test::disabled())
 {
-	constexpr int src_width = 640;
-    constexpr int src_height = 360;
+	int width = 640;
+	int height = 360;
 
-    for(int i = 10; i < 43; ++i)
-    {
-    string str = "/home/developer/ApraPipes/data/Raw_YUV420_640x360/Image0" + to_string(i) + "_YUV420.raw";
-    cout<<"Processing file: " << str << endl;
-    auto input_frame = makeYUV420Frame1(str, src_width, src_height);
-    BOOST_REQUIRE(input_frame != nullptr);
-    auto renderer = boost::make_shared<EglRenderer_test>(EglRendererProps(0, 0));
-    std::string inputPinId = "input";
-    framemetadata_sp metadata = input_frame->getMetadata();
-    //renderer->addInputPin(metadata);
+    FileReaderModuleProps fileReaderProps("/home/developer/ApraPipes/data/Raw_YUV420_640x360/Image001_YUV420.raw");
+	fileReaderProps.fps = 30;
+	auto fileReader = boost::shared_ptr<FileReaderModule>(new FileReaderModule(fileReaderProps));
+	auto metadata = framemetadata_sp(new RawImageMetadata(width, height, ImageMetadata::ImageType::UYVY, CV_8UC1, 0, CV_8U, FrameMetadata::MemType::DMABUF, true));
 
-    std::string outputPinId = "output";
-    frame_container frames;
-    frames[inputPinId] = input_frame;
+	auto rawImagePin = fileReader->addOutputPin(metadata);
 
-    renderer->processSOS(input_frame); 
-    renderer->process(frames);
-    renderer->processEOS(outputPinId);
-    }
+	auto sink = boost::shared_ptr<Module>(new EglRenderer(EglRendererProps(0, 0)));
+	fileReader->setNext(sink);
+
+	PipeLine p("test");
+	p.appendModule(fileReader);
+	BOOST_TEST(p.init());
+
+	Logger::setLogLevel(boost::log::trivial::severity_level::info);
+
+	p.run_all_threaded();
+
+	boost::this_thread::sleep_for(boost::chrono::seconds(10));
+	Logger::setLogLevel(boost::log::trivial::severity_level::error);
+
+	p.stop();
+	p.term();
+
+	p.wait_for_all();
 }
 
 BOOST_AUTO_TEST_CASE(displayOnTop, *boost::unit_test::disabled())
 {
-	NvV4L2CameraProps nvCamProps(640,360,10,false);
+	LoggerProps logProps;
+    logProps.enableConsoleLog = true;
+    Logger::initLogger(logProps);
+    Logger::setLogLevel(boost::log::trivial::severity_level::trace);
+	NvV4L2CameraProps nvCamProps(640,360, 10,false);
     auto source = boost::shared_ptr<Module>(new NvV4L2Camera(nvCamProps));
 
     NvTransformProps nvprops(ImageMetadata::RGBA);
@@ -63,7 +76,7 @@ BOOST_AUTO_TEST_CASE(displayOnTop, *boost::unit_test::disabled())
 
 BOOST_AUTO_TEST_CASE(switch_display, *boost::unit_test::disabled())
 {
-	NvV4L2CameraProps nvCamProps(640,36,10,false);
+	NvV4L2CameraProps nvCamProps(640,360, 10,false);
     auto source = boost::shared_ptr<Module>(new NvV4L2Camera(nvCamProps));
 
     NvTransformProps nvprops(ImageMetadata::RGBA);
@@ -92,7 +105,7 @@ BOOST_AUTO_TEST_CASE(switch_display, *boost::unit_test::disabled())
 
 BOOST_AUTO_TEST_CASE(open_close_window, *boost::unit_test::disabled())
 {
-	NvV4L2CameraProps nvCamProps(640,360,10,false);
+	NvV4L2CameraProps nvCamProps(640,360, 10,false);
     auto source = boost::shared_ptr<Module>(new NvV4L2Camera(nvCamProps));
 
     NvTransformProps nvprops(ImageMetadata::RGBA);
--- base/test/h264decoder_tests.cpp
+++ base/test/h264decoder_tests.cpp
@@ -5,7 +5,6 @@
 #include "Logger.h"
 #include "H264Decoder.h"
 #include "test_utils.h"
-#include "DMAFDToHostCopy.h"
 #include "PipeLine.h"
 #include "ExternalSinkModule.h"
 #include "H264Metadata.h"
@@ -16,18 +15,58 @@
 
 #include "CudaMemCopy.h"
 #include "nv_test_utils.h"
-
+#include "ResizeNPPI.h"
+#include "JPEGEncoderL4TM.h"
 
 BOOST_AUTO_TEST_SUITE(h264decoder_tests)
+
+BOOST_AUTO_TEST_CASE(mp4reader_decoder_eglrenderer_2)
+{
+	Logger::setLogLevel("debug");
+
+        auto fileReader =
+            boost::shared_ptr<FileReaderModule>(new FileReaderModule(
+                FileReaderModuleProps("./data/8bit_frame_1280x720_rgba.raw")));
+        auto metadata = framemetadata_sp(
+            new RawImageMetadata(1280, 720, ImageMetadata::ImageType::RGBA,
+                                 CV_8UC4, 0, CV_8U, FrameMetadata::HOST, true));
+        fileReader->addOutputPin(metadata);
+
+        auto stream = cudastream_sp(new ApraCudaStream);
+	auto memconversionDMA = boost::shared_ptr<Module>(new MemTypeConversion(MemTypeConversionProps(FrameMetadata::DMABUF, stream)));
+	fileReader->setNext(memconversionDMA);
+
+	auto memconversionHost = boost::shared_ptr<Module>(new MemTypeConversion(MemTypeConversionProps(FrameMetadata::HOST, stream)));
+	memconversionDMA->setNext(memconversionHost);
+
+	auto fileWriter = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps("./data/testOutput/MEMCPY_TEST/frame.raw",true)));
+	memconversionHost->setNext(fileWriter);
+
+	boost::shared_ptr<PipeLine> p;
+	p = boost::shared_ptr<PipeLine>(new PipeLine("test"));
+	p->appendModule(fileReader);
+
+        if (!p->init())
+	{
+		throw AIPException(AIP_FATAL, "Engine Pipeline init failed. Check IPEngine Logs for more details.");
+	}
+
+	p->run_all_threaded();
+	Test_Utils::sleep_for_seconds(1);
+	p->stop();
+	p->term();
+	p->wait_for_all();
+	p.reset();
+}
+
 
 BOOST_AUTO_TEST_CASE(mp4reader_decoder_eglrenderer)
 {
 	Logger::setLogLevel("info");
-	auto stream = cudastream_sp(new ApraCudaStream);
+
 	// metadata is known
-	std::string videoPath = "/home/developer/workspace/ApraPipes/1684824632.mp4";
-	// std::string videoPath = "/media/developer/7C3B-7A0B/2023-07-11/DOCTOR/PATIENT/2023-07-11_16-14-50-880.mp4";
-	auto mp4ReaderProps = Mp4ReaderSourceProps(videoPath, false);
+	std::string videoPath = "/home/developer/ApraPipes/data/Mp4_videos/h264_video/20221010/0012/1668063524439.mp4";
+	auto mp4ReaderProps = Mp4ReaderSourceProps(videoPath, false, 0, true, false, false);
 	auto mp4Reader = boost::shared_ptr<Mp4ReaderSource>(new Mp4ReaderSource(mp4ReaderProps));
 	auto h264ImageMetadata = framemetadata_sp(new H264Metadata(0, 0));
 	mp4Reader->addOutPutPin(h264ImageMetadata);
@@ -35,13 +74,7 @@ BOOST_AUTO_TEST_CASE(mp4reader_decoder_eglrenderer)
 	auto mp4Metadata = framemetadata_sp(new Mp4VideoMetadata("v_1"));
 	mp4Reader->addOutPutPin(mp4Metadata);
 
-	// StatSinkProps sinkProps;
-	// sinkProps.logHealth = true;
-	// sinkProps.logHealthFrequency = 100;
-	// auto sink2 = boost::shared_ptr<Module>(new StatSink(sinkProps));
-	// mp4Reader->setNext(sink2);
-
-	auto Decoder = boost::shared_ptr<H264Decoder>(new H264Decoder(H264DecoderProps()));
+	auto Decoder = boost::shared_ptr<Module>(new H264Decoder(H264DecoderProps()));
 	std::vector<std::string> mImagePin;
 	mImagePin = mp4Reader->getAllOutputPinsByType(FrameMetadata::FrameType::H264_DATA);
 	mp4Reader->setNext(Decoder, mImagePin);
@@ -51,7 +84,7 @@ BOOST_AUTO_TEST_CASE(mp4reader_decoder_eglrenderer)
 	Decoder->setNext(memconversion2);
 
 	
-	auto fileWriter = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps("./data/testOutput/yash_frame_????.raw")));
+	auto fileWriter = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps("./data/testOutput/ggg/yash_frame.raw",true)));
 	memconversion2->setNext(fileWriter);
 
 
@@ -74,5 +107,68 @@ BOOST_AUTO_TEST_CASE(mp4reader_decoder_eglrenderer)
 	p.reset();
 }
 
+/*
+BOOST_AUTO_TEST_CASE(sample_mp4_file_decoder_cuda_device_to_host)
+{
+	Logger::setLogLevel("info");
+
+	// metadata is known
+	std::string videoPath = "./data/Mp4_videos/h264_video/20221010/0012/1668064027062.mp4";
+	auto mp4ReaderProps = Mp4ReaderSourceProps(videoPath, false, 0, true, false, false);
+	auto mp4Reader = boost::shared_ptr<Mp4ReaderSource>(new Mp4ReaderSource(mp4ReaderProps));
+	auto h264ImageMetadata = framemetadata_sp(new H264Metadata(0, 0));
+	mp4Reader->addOutPutPin(h264ImageMetadata);
+
+	auto mp4Metadata = framemetadata_sp(new Mp4VideoMetadata("v_1"));
+	mp4Reader->addOutPutPin(mp4Metadata);
+
+	auto Decoder = boost::shared_ptr<Module>(new H264Decoder(H264DecoderProps()));
+	std::vector<std::string> mImagePin;
+	mImagePin = mp4Reader->getAllOutputPinsByType(FrameMetadata::FrameType::H264_DATA);
+	mp4Reader->setNext(Decoder, mImagePin);
+
+	auto stream = cudastream_sp(new ApraCudaStream);
+	auto memconversion2 = boost::shared_ptr<Module>(new MemTypeConversion(MemTypeConversionProps(FrameMetadata::HOST, stream)));
+	Decoder->setNext(memconversion2);
+
+	auto hostToDevice = boost::shared_ptr<CudaMemCopy>(new CudaMemCopy(CudaMemCopyProps(cudaMemcpyKind::cudaMemcpyHostToDevice, stream)));
+	memconversion2->setNext(hostToDevice);
+
+
+	auto resizeNPPI = boost::shared_ptr<Module>(new ResizeNPPI(ResizeNPPIProps(320, 180, stream)));
+	hostToDevice->setNext(resizeNPPI);
+
+
+	auto deviceToHost = boost::shared_ptr<CudaMemCopy>(new CudaMemCopy(CudaMemCopyProps(cudaMemcpyKind::cudaMemcpyDeviceToHost, stream)));
+	resizeNPPI->setNext(deviceToHost);
+	
+	auto jpegEncoder = boost::shared_ptr<JPEGEncoderL4TM>(new JPEGEncoderL4TM());
+	auto encodedImageMetadata = framemetadata_sp(new FrameMetadata(FrameMetadata::ENCODED_IMAGE));
+	auto encodedImagePin = jpegEncoder->addOutputPin(encodedImageMetadata);
+	deviceToHost->setNext(jpegEncoder);
+	
+	auto fileWriter = boost::shared_ptr<Module>(new FileWriterModule(FileWriterModuleProps("./data/testOutput/ULT_TEST/frame_????.jpg")));
+	jpegEncoder->setNext(fileWriter);
+
+
+	boost::shared_ptr<PipeLine> p;
+	p = boost::shared_ptr<PipeLine>(new PipeLine("test"));
+	p->appendModule(mp4Reader);
+
+	if (!p->init())
+	{
+		throw AIPException(AIP_FATAL, "Engine Pipeline init failed. Check IPEngine Logs for more details.");
+	}
+
+	p->run_all_threaded();
+
+	Test_Utils::sleep_for_seconds(15);
+
+	p->stop();
+	p->term();
+	p->wait_for_all();
+	p.reset();
+}
+*/
 
 BOOST_AUTO_TEST_SUITE_END()
